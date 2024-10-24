c6f7475fd2b4c576b72d662297f03937e60359af
Mike Jolley <mike.jolley@me.com>
2024-10-24 21:26:56 +0100

    Move placeholder logic to blocks

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/index.tsx
@@ -2,102 +2,85 @@
  * External dependencies
  */
 import clsx from 'clsx';
+import { useContext } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 import { TotalsItem } from '@woocommerce/blocks-components';
-import type { Currency } from '@woocommerce/types';
-import { ShippingVia } from '@woocommerce/base-components/cart-checkout/totals/shipping/shipping-via';
+import type { CartResponseTotals, CartShippingRate } from '@woocommerce/types';
 import {
-	isAddressComplete,
+	ShippingCalculator,
+	ShippingCalculatorContext,
+} from '@woocommerce/base-components/cart-checkout/shipping-calculator';
+import {
 	isPackageRateCollectable,
+	hasShippingRate,
+	getTotalShippingValue,
+	isAddressComplete,
 } from '@woocommerce/base-utils';
-import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
-import { useSelect } from '@wordpress/data';
-import { ShippingCalculatorContext } from '@woocommerce/base-components/cart-checkout/shipping-calculator/context';
+import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
 
 /**
  * Internal dependencies
  */
-import { ShippingCalculator } from '../../shipping-calculator';
-import {
-	hasShippingRate,
-	getTotalShippingValue,
-	areShippingMethodsMissing,
-} from './utils';
-import ShippingPlaceholder from './shipping-placeholder';
-import ShippingAddress from './shipping-address';
-import ShippingRateSelector from './shipping-rate-selector';
+import { ShippingVia } from './shipping-via';
+import { ShippingAddress } from './shipping-address';
+import { ShippingRateSelector } from './shipping-rate-selector';
 import './style.scss';
 
 export interface TotalShippingProps {
-	currency: Currency;
-	values: {
-		total_shipping: string;
-		total_shipping_tax: string;
-	}; // Values in use
-	showCalculator?: boolean; //Whether to display the rate selector below the shipping total.
-	showRateSelector?: boolean; // Whether to show shipping calculator or not.
+	shippingRates: CartShippingRate[];
+	values: CartResponseTotals;
 	className?: string;
-	isCheckout?: boolean;
+	placeholder?: React.ReactNode | null;
+	showRateSelector?: boolean;
 }
-export const TotalsShipping = ( {
-	currency,
-	values,
-	showCalculator = true,
-	showRateSelector = true,
-	isCheckout = false,
-	className,
-}: TotalShippingProps ): JSX.Element => {
-	const [ isShippingCalculatorOpen, setIsShippingCalculatorOpen ] =
-		useState( false );
-	const {
-		shippingAddress,
-		cartHasCalculatedShipping,
-		shippingRates,
-		isLoadingRates,
-	} = useStoreCart();
-	const totalShippingValue = getTotalShippingValue( values );
-	const hasRates = hasShippingRate( shippingRates ) || totalShippingValue > 0;
-	const showShippingCalculatorForm =
-		showCalculator && isShippingCalculatorOpen;
 
-	const prefersCollection = useSelect( ( select ) => {
-		return select( CHECKOUT_STORE_KEY ).prefersCollection();
-	} );
-	const selectedShippingRates = shippingRates.flatMap(
-		( shippingPackage ) => {
-			return shippingPackage.shipping_rates
-				.filter(
-					( rate ) =>
-						// If the shopper prefers collection, the rate is collectable AND selected.
-						( prefersCollection &&
-							isPackageRateCollectable( rate ) &&
-							rate.selected ) ||
-						// Or the shopper does not prefer collection and the rate is selected
-						( ! prefersCollection && rate.selected )
-				)
-				.flatMap( ( rate ) => rate.name );
-		}
+const renderShippingTotalValue = ( value: number ) => {
+	if ( value === 0 ) {
+		return <strong>{ __( 'Free', 'woocommerce' ) }</strong>;
+	}
+	return value;
+};
+
+export const TotalsShipping = ( {
+	shippingRates,
+	values,
+	placeholder = null,
+	className,
+	showRateSelector = false,
+}: TotalShippingProps ): JSX.Element | null => {
+	const { shippingAddress, cartHasCalculatedShipping, isLoadingRates } =
+		useStoreCart();
+	const { isShippingCalculatorOpen } = useContext(
+		ShippingCalculatorContext
 	);
-	const addressComplete = isAddressComplete( shippingAddress, [
+	const totalShippingValue = getTotalShippingValue( values );
+	const totalCurrency = getCurrencyFromPriceResponse( values );
+	const hasRates =
+		cartHasCalculatedShipping &&
+		( hasShippingRate( shippingRates ) || totalShippingValue > 0 );
+	const hasCompleteAddress = isAddressComplete( shippingAddress, [
 		'state',
 		'country',
 		'postcode',
 		'city',
 	] );
-	const shippingMethodsMissing = areShippingMethodsMissing(
-		hasRates,
-		prefersCollection,
-		shippingRates
-	);
 
-	const valueToDisplay =
-		totalShippingValue === 0 ? (
-			<strong>{ __( 'Free', 'woocommerce' ) }</strong>
-		) : (
-			totalShippingValue
-		);
+	const selectedShippingRates = shippingRates.flatMap(
+		( shippingPackage ) => {
+			return shippingPackage.shipping_rates
+				.filter( ( rate ) => rate.selected )
+				.flatMap( ( rate ) => rate.name );
+		}
+	);
+	const isCollectionOnly = hasRates
+		? shippingRates.every( ( shippingPackage ) => {
+				return shippingPackage.shipping_rates.every(
+					( rate ) =>
+						! rate.selected || isPackageRateCollectable( rate )
+				);
+		  } )
+		: false;
 
 	return (
 		<div
@@ -106,33 +89,20 @@ export const TotalsShipping = ( {
 				className
 			) }
 		>
-			<ShippingCalculatorContext.Provider
-				value={ {
-					showCalculator,
-					shippingCalculatorID: 'shipping-calculator-form-wrapper',
-					isShippingCalculatorOpen,
-					setIsShippingCalculatorOpen,
-				} }
-			>
-				<TotalsItem
-					label={ __( 'Delivery', 'woocommerce' ) }
-					value={
-						! shippingMethodsMissing && cartHasCalculatedShipping
-							? // if address is not complete, display the link to add an address.
-							  valueToDisplay
-							: ( ! addressComplete || isCheckout ) && (
-									<ShippingPlaceholder
-										showCalculator={ showCalculator }
-										isCheckout={ isCheckout }
-										addressProvided={ addressComplete }
-									/>
-							  )
-					}
-					description={
-						( ! shippingMethodsMissing &&
-							cartHasCalculatedShipping ) ||
-						// If address is complete, display the shipping address.
-						( addressComplete && ! isCheckout ) ? (
+			<TotalsItem
+				label={
+					isCollectionOnly
+						? __( 'Collection', 'woocommerce' )
+						: __( 'Delivery', 'woocommerce' )
+				}
+				value={
+					hasRates
+						? renderShippingTotalValue( totalShippingValue )
+						: placeholder
+				}
+				description={
+					<>
+						{ hasRates && (
 							<>
 								<ShippingVia
 									selectedShippingRates={
@@ -143,23 +113,22 @@ export const TotalsShipping = ( {
 									shippingAddress={ shippingAddress }
 								/>
 							</>
-						) : null
-					}
-					currency={ currency }
-				/>
-				{ showShippingCalculatorForm && <ShippingCalculator /> }
-				{ showRateSelector &&
-					cartHasCalculatedShipping &&
-					! showShippingCalculatorForm && (
-						<ShippingRateSelector
-							hasRates={ hasRates }
-							shippingRates={ shippingRates }
-							isLoadingRates={ isLoadingRates }
-							isAddressComplete={ addressComplete }
-							shippingAddress={ shippingAddress }
-						/>
-					) }
-			</ShippingCalculatorContext.Provider>
+						) }
+						{ isShippingCalculatorOpen && <ShippingCalculator /> }
+						{ ! isShippingCalculatorOpen &&
+							showRateSelector &&
+							hasRates && (
+								<ShippingRateSelector
+									shippingRates={ shippingRates }
+									isLoadingRates={ isLoadingRates }
+									hasCompleteAddress={ hasCompleteAddress }
+									shippingAddress={ shippingAddress }
+								/>
+							) }
+					</>
+				}
+				currency={ totalCurrency }
+			/>
 		</div>
 	);
 };
