d14584408d3f83e1dd8d4977f897f0f81fb91bf4
Vladimir Reznichenko <kalessil@gmail.com>
2024-10-25 17:05:12 +0200

    Monorepo: cleanup.

--- plugins/woocommerce/bin/build-zip.sh
+++ plugins/woocommerce/bin/build-zip.sh
@@ -22,7 +22,7 @@ if [ -z "${NODE_ENV}" ]; then
 fi
 pnpm --filter='@woocommerce/plugin-woocommerce' build || exit "$?"
 echo "Cleaning up PHP dependencies..."
-composer config autoload-dev --unset || exit "$?"
+composer config autoload-dev.files --unset || exit "$?"
 composer install --no-dev --quiet --optimize-autoloader || exit "$?"
 echo "Run makepot..."
 pnpm --filter=@woocommerce/plugin-woocommerce makepot || exit "$?"
