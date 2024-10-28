8677c34aa621b3e69f88912e9512ff4e35a54e8c
 Ján Mikláš <neosinner@gmail.com>
2024-10-28 18:03:07 +0100

    Render "Processing order" with dummy order in email preview

--- plugins/woocommerce/src/Internal/Admin/EmailPreview.php
+++ plugins/woocommerce/src/Internal/Admin/EmailPreview.php
@@ -7,7 +7,10 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\Internal\Admin;
 
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 use WC_Email;
+use WC_Order;
+use WC_Product;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -41,6 +44,10 @@ class EmailPreview {
 	 * @return string
 	 */
 	public function render() {
+		if ( FeaturesUtil::feature_is_enabled( 'email_improvements' ) ) {
+			return $this->render_preview_email();
+		}
+
 		return $this->render_legacy_preview_email();
 	}
 
@@ -70,5 +77,80 @@ class EmailPreview {
 		 * @since 2.6.0
 		 */
 		return apply_filters( 'woocommerce_mail_content', $email->style_inline( $mailer->wrap_message( $email_heading, $message ) ) );
+	}
+
+	/**
+	 * Render HTML content of the preview email.
+	 *
+	 * @return string
+	 */
+	private function render_preview_email() {
+		$email = $this->get_email();
+
+		$order = $this->get_dummy_order();
+		$email->set_object( $order );
+
+		$content = $email->get_content_html();
+
+		/**
+		 * Wrap the content with the email template and then add styles.
+		 *
+		 * @since 9.5.0
+		 */
+		return apply_filters( 'woocommerce_mail_content', $email->style_inline( $content ) );
+	}
+
+	/**
+	 * Get a dummy order object without the need to create in the database.
+	 *
+	 * @return WC_Order
+	 */
+	private function get_dummy_order() {
+		$product = new WC_Product();
+		$product->set_name( 'Dummy Product' );
+		$product->set_price( 25 );
+
+		$order = new WC_Order();
+		$order->add_product( $product, 2 );
+		$order->set_id( 12345 );
+		$order->set_date_created( time() );
+		$order->set_currency( 'USD' );
+		$order->set_total( 100 );
+
+		$address = $this->get_dummy_address();
+		$order->set_billing_address( $address );
+
+		return $order;
+	}
+
+	/**
+	 * Get a dummy address.
+	 *
+	 * @return array
+	 */
+	private function get_dummy_address() {
+		return array(
+			'first_name' => 'John',
+			'last_name'  => 'Doe',
+			'company'    => 'Company',
+			'email'      => 'john@company.com',
+			'phone'      => '555-555-5555',
+			'address_1'  => '123 Fake Street',
+			'city'       => 'Faketown',
+			'postcode'   => '12345',
+			'country'    => 'US',
+			'state'      => 'CA',
+		);
+	}
+
+	/**
+	 * Get the email class for email preview.
+	 *
+	 * @return WC_Email
+	 */
+	private function get_email() {
+		$emails = WC()->mailer()->get_emails();
+		$email  = $emails['WC_Email_Customer_Processing_Order'];
+		return $email;
 	}
 }
