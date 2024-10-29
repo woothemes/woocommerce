bbef2ce4c820dfe426695bdb1ae70e8266d92ce2
paul sealock <psealock@gmail.com>
2024-10-29 14:52:38 +1300

    remove style

--- packages/js/settings-editor/src/index.tsx
+++ packages/js/settings-editor/src/index.tsx
@@ -3,8 +3,6 @@
  */
 import { createElement } from '@wordpress/element';
 
-import './style.scss';
-
 export const SettingsEditor = () => {
 	return <div style={ { padding: '20px' } }>Settings Editor</div>;
 };
