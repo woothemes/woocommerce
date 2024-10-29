e6fb46ca310403b08bdf4991a7f7015b3169b054
Vladimir Reznichenko <kalessil@gmail.com>
2024-10-29 09:01:27 +0100

    Monorepo: cleanup.

--- plugins/woocommerce/bin/post-install.sh
+++ plugins/woocommerce/bin/post-install.sh
@@ -3,7 +3,7 @@
 # Required for dev and build environments: generate optimized autoloaders, safe to run in background.
 composer dump-autoload --optimize --quiet &
 
-if [[ -z ${CI+x} ]]; then
+if [ -z ${CI+y} ]; then
 	# Required for dev-environments: install tooling dependencies in background
     composer bin all install --ansi &
 else
