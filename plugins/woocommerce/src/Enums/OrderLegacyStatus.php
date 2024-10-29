9ce60f087965a7db38da306d3ee8475376e16043
Wesley Rosa <wesleyjrosa@gmail.com>
2024-10-29 14:30:14 -0300

    Introducing order legacy status constants

---
+++ plugins/woocommerce/src/Enums/OrderLegacyStatus.php
@@ -0,0 +1,68 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Enums;
+
+use Automattic\WooCommerce\Blocks\Domain\Services\DraftOrders;
+
+/**
+ * Enum class for all the legacy order statuses.
+ */
+final class OrderLegacyStatus {
+	/**
+	 * The order is pending payment.
+	 *
+	 * @var string
+	 */
+	const PENDING = 'wc-pending';
+
+	/**
+	 * The order is processing.
+	 *
+	 * @var string
+	 */
+	const PROCESSING = 'wc-processing';
+
+	/**
+	 * The order is on hold.
+	 *
+	 * @var string
+	 */
+	const ON_HOLD = 'wc-on-hold';
+
+	/**
+	 * The order is completed.
+	 *
+	 * @var string
+	 */
+	const COMPLETED = 'wc-completed';
+
+	/**
+	 * The order is cancelled.
+	 *
+	 * @var string
+	 */
+	const CANCELLED = 'wc-cancelled';
+
+	/**
+	 * The order is refunded.
+	 *
+	 * @var string
+	 */
+	const REFUNDED = 'wc-refunded';
+
+	/**
+	 * The order is failed.
+	 *
+	 * @var string
+	 */
+	const FAILED = 'wc-failed';
+
+	/**
+	 * The order is a draft.
+	 *
+	 * @var string
+	 */
+	const DRAFT = DraftOrders::DB_STATUS;
+}
