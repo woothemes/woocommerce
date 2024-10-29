f27ae34305e0ca33c47ec295955d1af0e4aae6a2
Fernando Marichal <fernando.marichal@automattic.com>
2024-10-29 18:12:13 -0300

    Add templates version

--- plugins/woocommerce/templates/single-product/related.php
+++ plugins/woocommerce/templates/single-product/related.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     3.9.0
+ * @version     9.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -32,7 +32,7 @@ if ( $related_products ) : ?>
 			?>
 			<h2><?php echo esc_html( $heading ); ?></h2>
 		<?php endif; ?>
-		
+
 		<?php woocommerce_product_loop_start(); ?>
 
 			<?php foreach ( $related_products as $related_product ) : ?>
