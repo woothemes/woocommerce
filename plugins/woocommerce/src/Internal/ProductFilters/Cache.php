dfa703b5b8c8406387f0b74ea76b3c687ee85179
Tung Du <dinhtungdu@gmail.com>
2024-10-30 15:42:55 +0700

    feat: cache subquery results

--- plugins/woocommerce/src/Internal/ProductFilters/Cache.php
+++ plugins/woocommerce/src/Internal/ProductFilters/Cache.php
@@ -76,4 +76,58 @@ class Cache {
 	public function get_object_cache_key( string $id ) {
 		return WC_Cache_Helper::get_cache_prefix( self::CACHE_GROUP ) . $id;
 	}
+
+	/**
+	 * Get cached product IDs from a SQL query.
+	 *
+	 * Executes the product query and returns a comma-separated string of product IDs.
+	 * Results are cached to avoid repeated database queries.
+	 *
+	 * @param string $product_query_sql SQL query to get product IDs.
+	 * @return string Comma-separated list of product IDs.
+	 */
+	public function get_cached_product_ids( $product_query_sql ) {
+		return $this->get_cached_subquery(
+			$product_query_sql,
+			function ( $product_query_sql ) {
+				global $wpdb;
+
+				$results = $wpdb->get_results( $product_query_sql, ARRAY_A );
+
+				if ( ! $results ) {
+					$results = array();
+				}
+				return implode( ',', array_column( $results, 'ID' ) );
+			}
+		);
+	}
+
+	/**
+	 * Get cached results for any SQL subquery.
+	 *
+	 * Checks object cache for results first. If not found, executes the callback
+	 * function to run the query and caches the results.
+	 *
+	 * @param string   $sql_query SQL query string.
+	 * @param callable $callback  Callback function to execute the query if not cached.
+	 * @return mixed|null Cached query results or null if no callback provided.
+	 */
+	public function get_cached_subquery( string $sql_query, $callback ) {
+		$cache_key = $this->get_object_cache_key( md5( $sql_query ) );
+		$cache     = wp_cache_get( $cache_key );
+
+		if ( $cache ) {
+			return $cache;
+		}
+
+		if ( ! $callback ) {
+			return null;
+		}
+
+		$results = call_user_func( $callback, $sql_query );
+
+		wp_cache_set( $cache_key, $results );
+
+		return $results;
+	}
 }
