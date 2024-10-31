b773653347189aea543d5f18ec96ae6a331d3364
Damián Suárez <rdsuarez@gmail.com>
2024-10-31 18:40:06 +0000

    sort attribute colors

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/checkbox-list/constants.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/checkbox-list/constants.ts
@@ -4,7 +4,7 @@
 import type { BlockAttributes } from './types';
 
 export const colorAttributes: Array< keyof BlockAttributes > = [
+	'optionElement',
 	'optionElementBorder',
 	'optionElementSelected',
-	'optionElement',
 ];
