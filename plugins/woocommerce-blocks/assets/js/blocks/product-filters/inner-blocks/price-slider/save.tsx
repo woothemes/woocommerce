ae8317f30355debb8137a41f844718f2c2f538ef
Damián Suárez <rdsuarez@gmail.com>
2024-10-31 22:49:03 +0000

    save CSS classes and style attributes

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/price-slider/save.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/price-slider/save.tsx
@@ -2,9 +2,36 @@
  * External dependencies
  */
 import { useBlockProps } from '@wordpress/block-editor';
+import clsx from 'clsx';
 
-export default function save() {
-	const blockProps = useBlockProps.save();
+/**
+ * Internal dependencies
+ */
+import { getHasColorClasses, getStyleColorVars } from '../../utils/colors';
+import { colorAttributes } from './constants';
+import type { BlockAttributes } from './types';
+
+export default function save( {
+	attributes,
+	style,
+}: {
+	attributes: BlockAttributes;
+	style: Record< string, string >;
+} ) {
+	const blockProps = useBlockProps.save( {
+		className: clsx(
+			'wc-block-product-filter-price-slider',
+			getHasColorClasses( attributes, colorAttributes )
+		),
+		style: {
+			...style,
+			...getStyleColorVars(
+				'wc-product-filter--price',
+				attributes,
+				colorAttributes
+			),
+		},
+	} );
 
 	return <div { ...blockProps } />;
 }
