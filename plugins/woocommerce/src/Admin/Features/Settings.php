323a3b9a3a68fd32a4f34aa1980f7dba96c1f88f
paul sealock <psealock@gmail.com>
2024-11-04 11:29:46 +1300

    enqueue_settings_editor_styles

--- plugins/woocommerce/src/Admin/Features/Settings.php
+++ plugins/woocommerce/src/Admin/Features/Settings.php
@@ -38,6 +38,7 @@ class Settings {
 
 		add_filter( 'woocommerce_admin_shared_settings', array( __CLASS__, 'add_component_settings' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_settings_editor_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_settings_editor_styles' ) );
 	}
 
 	/**
@@ -48,6 +49,40 @@ class Settings {
 	public function is_settings_page() {
 		$screen = get_current_screen();
 		return $screen && 'woocommerce_page_wc-settings' === $screen->id;
+	}
+
+	public function enqueue_settings_editor_styles() {
+		$style_name            = 'wc-admin-edit-settings';
+		$style_path_name       = 'settings';
+		$style_assets_filename = WCAdminAssets::get_script_asset_filename( $style_path_name, 'style' );
+		$style_assets          = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . $style_path_name . '/' . $style_assets_filename;
+
+		// Settings Editor styles.
+		wp_register_style(
+			$style_name,
+			WCAdminAssets::get_url( $style_path_name . '/style', 'css' ),
+			isset( $style_assets['dependencies'] ) ? $style_assets['dependencies'] : array(),
+			WCAdminAssets::get_file_version( 'css', $style_assets['version'] ),
+		);
+
+		wp_enqueue_style( $style_name );
+
+		// Global presets styles.
+		wp_register_style( 'wc-global-presets', false ); // phpcs:ignore
+		wp_add_inline_style( 'wc-global-presets', wp_get_global_stylesheet( array( 'presets' ) ) );
+		wp_enqueue_style( 'wc-global-presets' );
+
+		// Gutenberg posts editor styles.
+		if ( function_exists( 'gutenberg_url' ) ) {
+			// phpcs:disable WordPress.WP.EnqueuedResourceParameters.MissingVersion
+			wp_register_style(
+				'wp-gutenberg-posts-dashboard',
+				gutenberg_url( 'build/edit-site/posts.css', __FILE__ ),
+				array( 'wp-components' ),
+			);
+			// phpcs:enable WordPress.WP.EnqueuedResourceParameters.MissingVersion
+			wp_enqueue_style( 'wp-gutenberg-posts-dashboard' );
+		}
 	}
 
 	/**
@@ -65,8 +100,6 @@ class Settings {
 		$script_path_name       = 'settings';
 		$script_assets_filename = WCAdminAssets::get_script_asset_filename( $script_path_name, 'index' );
 		$script_assets          = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . $script_path_name . '/' . $script_assets_filename;
-		$style_assets_filename  = WCAdminAssets::get_script_asset_filename( $script_path_name, 'style' );
-		$style_assets           = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . $script_path_name . '/' . $style_assets_filename;
 
 		wp_enqueue_script(
 			$script_name,
@@ -75,30 +108,6 @@ class Settings {
 			WCAdminAssets::get_file_version( 'js', $script_assets['version'] ),
 			true
 		);
-
-		wp_register_style(
-			$script_name,
-			WCAdminAssets::get_url( $script_path_name . '/style', 'css' ),
-			isset( $style_assets['dependencies'] ) ? $style_assets['dependencies'] : array(),
-			WCAdminAssets::get_file_version( 'css', $style_assets['version'] ),
-		);
-
-		wp_enqueue_style( $script_name );
-
-		wp_register_style( 'wc-global-presets', false ); // phpcs:ignore
-		wp_add_inline_style( 'wc-global-presets', wp_get_global_stylesheet( array( 'presets' ) ) );
-		wp_enqueue_style( 'wc-global-presets' );
-
-		if ( function_exists( 'gutenberg_url' ) ) {
-			// phpcs:disable WordPress.WP.EnqueuedResourceParameters.MissingVersion
-			wp_register_style(
-				'wp-gutenberg-posts-dashboard',
-				gutenberg_url( 'build/edit-site/posts.css', __FILE__ ),
-				array( 'wp-components' ),
-			);
-			// phpcs:enable WordPress.WP.EnqueuedResourceParameters.MissingVersion
-			wp_enqueue_style( 'wp-gutenberg-posts-dashboard' );
-		}
 	}
 
 	/**
