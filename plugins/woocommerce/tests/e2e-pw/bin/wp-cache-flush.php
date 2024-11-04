e40fd8f8df757c417f149a46b044198e0766a4e0
Jon Lane <jon.lane@automattic.com>
2024-11-04 13:50:04 -0800

    Resolve new linting errors

--- plugins/woocommerce/tests/e2e-pw/bin/wp-cache-flush.php
+++ plugins/woocommerce/tests/e2e-pw/bin/wp-cache-flush.php
@@ -1,10 +1,12 @@
 <?php
-declare(strict_types=1);
 /**
  * Plugin name: WP Cache Flush
  * Description: Flush cache for E2E test runs.
  *
  * @package Automattic\WooCommerce\E2EPlaywright
+ *
+ * Clears the cache for E2E test runs.
  */
+declare(strict_types=1);
 
 wp_cache_flush();
