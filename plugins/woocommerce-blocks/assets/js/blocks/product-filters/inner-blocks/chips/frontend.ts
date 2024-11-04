0da96945d4f9979a47aac6447e1f9d42367154a7
Tung Du <dinhtungdu@gmail.com>
2024-11-04 21:12:49 +0700

    feat: simplify chips block

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/chips/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/chips/frontend.ts
@@ -6,41 +6,40 @@ import { getElement, getContext, store } from '@woocommerce/interactivity';
 /**
  * Internal dependencies
  */
+import { ProductFiltersContext } from '../../frontend';
 
 export type ChipsContext = {
-	items: {
-		id: string;
-		label: string;
-		value: string;
-		checked: boolean;
-	}[];
 	showAll: boolean;
+	filterParamKeys: string[];
 };
 
 store( 'woocommerce/product-filter-chips', {
+	state: {
+		get isItemSelected() {
+			const context = getContext< ChipsContext >();
+
+			if (
+				! context.filterParamKeys ||
+				context.filterParamKeys.length === 0
+			) {
+				return false;
+			}
+
+			const { props } = getElement();
+			const productFiltersContext = getContext< ProductFiltersContext >(
+				'woocommerce/product-filters'
+			);
+			return context.filterParamKeys.some(
+				( key ) =>
+					key in productFiltersContext.params &&
+					productFiltersContext.params[ key ].includes( props.value )
+			);
+		},
+	},
 	actions: {
 		showAllItems: () => {
 			const context = getContext< ChipsContext >();
 			context.showAll = true;
-		},
-
-		selectItem: () => {
-			const { ref } = getElement();
-			const value = ref.getAttribute( 'value' );
-
-			if ( ! value ) return;
-
-			const context = getContext< ChipsContext >();
-
-			context.items = context.items.map( ( item ) => {
-				if ( item.value.toString() === value ) {
-					return {
-						...item,
-						checked: ! item.checked,
-					};
-				}
-				return item;
-			} );
 		},
 	},
 } );
