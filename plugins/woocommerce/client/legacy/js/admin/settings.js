f6b6f7620b3b08ca714f78607c3767844c13d16c
alex-mpoet <alex-mpoet@users.noreply.github.com>
2024-11-05 23:13:37 +0300

    Add js, css and image assets for the image url field

--- plugins/woocommerce/client/legacy/js/admin/settings.js
+++ plugins/woocommerce/client/legacy/js/admin/settings.js
@@ -252,7 +252,7 @@
 					.attr( { tabindex: '-1', 'aria-hidden': 'true' } );
 				if ( ! data.isInitialLoad ) {
 					$( '.woocommerce-save-button' ).removeAttr( 'disabled' );
-				}	
+				}
 			} );
 
 		$( '.wc-item-reorder-nav' )
@@ -305,6 +305,45 @@
 			if ( ! $( this ).attr( 'disabled' ) ) {
 				$( this ).addClass( 'is-busy' );
 			}
+		} );
+
+		$( '.woocommerce .select_image' ).on( 'click', function ( e ) {
+			e.preventDefault();
+			if ( wp.media.frames.img_select ) {
+				wp.media.frames.img_select.open();
+				return;
+			}
+			wp.media.frames.img_select = wp.media({
+        library: {
+          type: 'image'
+        }
+			});
+			const select_image_button = $( this );
+			const select_image = function () {
+        const sel = wp.media.frames.img_select.state()
+          .get( 'selection' );
+        if ( ! sel ) {
+          return;
+        }
+        sel.each(function (item) {
+          const wrapper = select_image_button.closest( '.forminp' );
+          wrapper.find( 'input.image_url' ).val( item.attributes.url ).trigger( 'change' );
+          wrapper.find( '.image_preview' ).attr( 'src', item.attributes.url );
+          wrapper.find( '.new_image' ).addClass( 'hidden' );
+          wrapper.find( '.existing_image' ).removeClass( 'hidden' );
+        });
+			};
+			wp.media.frames.img_select.on( 'select', select_image );
+			wp.media.frames.img_select.open();
+		} );
+
+		$( '.woocommerce .remove_image' ).on( 'click', function ( e ) {
+			e.preventDefault();
+			const remove_image_button = $( this );
+			const wrapper = remove_image_button.closest( '.forminp' );
+			wrapper.find( 'input.image_url' ).val( '' ).trigger( 'change' );
+			wrapper.find( '.new_image' ).removeClass( 'hidden' );
+			wrapper.find( '.existing_image' ).addClass( 'hidden' );
 		} );
 
 		/**
