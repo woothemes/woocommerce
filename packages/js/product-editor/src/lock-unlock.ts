c6191b7c2238139463d74b1224d9e1db36ab7765
paul sealock <psealock@gmail.com>
2024-11-06 17:02:17 +1300

    format

--- packages/js/product-editor/src/lock-unlock.ts
+++ packages/js/product-editor/src/lock-unlock.ts
@@ -14,8 +14,8 @@ const noop = () => {};
 const { lock, unlock } = isGutenbergEnabled
 	? __dangerousOptInToUnstableAPIsOnlyForCoreModules(
 			'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.',
-		'@wordpress/edit-site'
-	)
+			'@wordpress/edit-site'
+	  )
 	: { lock: noop, unlock: noop };
 
 export { lock, unlock };
