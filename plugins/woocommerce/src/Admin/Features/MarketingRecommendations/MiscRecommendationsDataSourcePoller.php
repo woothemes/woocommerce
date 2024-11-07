ce9e2a81d5ecea4996019517c1f0a7f5112d868b
Ian Yu-Hsun Lin <ianlin@automattic.com>
2024-11-07 20:26:10 +0800

    Add a marketing API to get the misc recommendation from woocommerce.com

---
+++ plugins/woocommerce/src/Admin/Features/MarketingRecommendations/MiscRecommendationsDataSourcePoller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\Features\MarketingRecommendations;
+
+use Automattic\WooCommerce\Admin\RemoteSpecs\DataSourcePoller;
+use WC_Helper;
+
+/**
+ * Specs data source poller class for misc recommendations.
+ *
+ * @since 9.5.0
+ */
+class MiscRecommendationsDataSourcePoller extends DataSourcePoller {
+
+	/**
+	 * Data Source Poller ID.
+	 */
+	const ID = 'misc_recommendations';
+
+	/**
+	 * Class instance.
+	 *
+	 * @var MiscRecommendationsDataSourcePoller instance
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Get class instance.
+	 */
+	public static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new self(
+				self::ID,
+				self::get_data_sources(),
+			);
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Get data sources.
+	 *
+	 * @return array
+	 */
+	public static function get_data_sources() {
+		return array(
+			WC_Helper::get_woocommerce_com_base_url() . 'wp-json/wccom/marketing-tab/misc/recommendations.json',
+		);
+	}
+}
