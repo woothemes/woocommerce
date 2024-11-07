f02195b433188da38aeeba78305e0199e932a5f7
“Joshua <joshuatf@gmail.com>
2024-11-07 15:13:41 -0500

    Save deltas to json file

--- tools/compare-perf/calculate-delta.ts
+++ tools/compare-perf/calculate-delta.ts
@@ -2,6 +2,7 @@
  * External dependencies
  */
 const bold = require( 'chalk' );
+const fs = require( 'fs' );
 const path = require( 'path' );
 
 /**
@@ -15,7 +16,8 @@ const formats = {
 
 const ARTIFACTS_PATH =
 	process.env.WP_ARTIFACTS_PATH || path.join( process.cwd(), 'artifacts' );
-const RESULTS_FILE_SUFFIX = '.performance-results.json';
+const PERFORMANCE_FILE_SUFFIX = '.performance-results.json';
+const DELTAS_FILE_SUFFIX = '.delta-results.json';
 
 /**
  * Calculates and prints the deltas for server response time for each test suite.
@@ -30,25 +32,46 @@ function calculateDelta( testSuites: string[], branches: string[] ): void {
 	for ( const testSuite of testSuites ) {
 		logAtIndent( 1, 'Test suite:', formats.success( testSuite ) );
 
-		const calculatedResultsPath = path.join(
+		const performanceResultsPath = path.join(
 			ARTIFACTS_PATH,
-			testSuite + RESULTS_FILE_SUFFIX
+			testSuite + PERFORMANCE_FILE_SUFFIX
 		);
 
-		const results = readJSONFile( calculatedResultsPath, 'utf8' );
+		const deltaMetrics = {} as { [ key: string ]: number };
+		const performanceResults = readJSONFile(
+			performanceResultsPath,
+			'utf8'
+		);
 		const currentBranch = branches[ 0 ];
 		const baseBranch = branches[ 1 ];
 
 		const percentage =
-			( results[ currentBranch ].serverResponse /
-				results[ baseBranch ].serverResponse -
+			( performanceResults[ currentBranch ].serverResponse /
+				performanceResults[ baseBranch ].serverResponse -
 				1 ) *
 			100;
+
+		deltaMetrics.serverResponse = percentage;
 
 		logAtIndent(
 			2,
 			'Server response delta:',
 			formats.success( percentage.toFixed( 2 ) + '%' )
+		);
+
+		const deltaResultsPath = path.join(
+			ARTIFACTS_PATH,
+			testSuite + DELTAS_FILE_SUFFIX
+		);
+
+		logAtIndent(
+			2,
+			'Saving curated results to:',
+			formats.success( deltaResultsPath )
+		);
+		fs.writeFileSync(
+			deltaResultsPath,
+			JSON.stringify( deltaMetrics, null, 2 )
 		);
 	}
 }
