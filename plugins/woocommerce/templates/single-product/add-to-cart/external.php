851e58cde9298aafbec9c452adea5f3f14caf484
Damián Suárez <rdsuarez@gmail.com>
2024-11-07 14:15:04 +0000

    add input and button wrapper for externals

--- plugins/woocommerce/templates/single-product/add-to-cart/external.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/external.php
@@ -20,6 +20,9 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
 <form class="cart" action="<?php echo esc_url( $product_url ); ?>" method="get">
+	<?php do_action( 'woocommerce_before_add_to_cart_quantity_and_button' ); ?>
+	<div class="woocommerce_after_add_to_cart_quantity_and_button">
+
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 	<button type="submit" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $button_text ); ?></button>
@@ -27,6 +30,9 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php wc_query_string_form_fields( $product_url ); ?>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+	</div>
+	<?php do_action( 'woocommerce_after_add_to_cart_quantity_and_button' ); ?>
 </form>
 
 <?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
--- plugins/woocommerce/templates/single-product/add-to-cart/simple.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/simple.php
@@ -32,10 +32,7 @@ if ( $product->is_in_stock() ) : ?>
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-		<?php
-			do_action( 'woocommerce_before_add_to_cart_quantity_and_button' );
-		?>
-
+		<?php do_action( 'woocommerce_before_add_to_cart_quantity_and_button' ); ?>
 		<div class="woocommerce_after_add_to_cart_quantity_and_button">
 
 		<?php
