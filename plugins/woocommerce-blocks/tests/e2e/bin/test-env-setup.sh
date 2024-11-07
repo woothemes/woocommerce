b7688113ef68d7c5da86064f1c8acfd4f23c5666
Alex Florisca <alex.florisca@automattic.com>
2024-11-07 10:19:24 +0000

    Fix ci translations and tidy up scripts

--- plugins/woocommerce-blocks/tests/e2e/bin/test-env-setup.sh
+++ plugins/woocommerce-blocks/tests/e2e/bin/test-env-setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Remove the database snapshot if it exists.
 wp-env run tests-cli -- rm -f blocks_e2e.sql
@@ -7,9 +8,5 @@ wp-env run tests-cli -- bash wp-content/plugins/woocommerce/blocks-bin/playwrigh
 # Disable the LYS Coming Soon banner.
 wp-env run tests-cli -- wp option update woocommerce_coming_soon 'no'
 
-#!/bin/sh
-BASENAME=$(basename "`pwd`")
-# We need to pass the blocks plugin folder name to the script, the name can change depending on your local env and we can't hardcode it.
-wp-env run tests-cli './wp-content/plugins/woocommerce/blocks-bin/wp-env-config.sh' woocommerce
-
-node ./tests/e2e/bin/generate-test-translations.js
+echo "Generating test translations"
+node $script_dir/generate-test-translations.js
