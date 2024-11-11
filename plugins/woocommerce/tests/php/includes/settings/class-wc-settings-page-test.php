2f7d61240f749a8249aa3cc8df21659050fc12d4
paul sealock <psealock@gmail.com>
2024-11-11 17:37:39 +1300

    lint

--- plugins/woocommerce/tests/php/includes/settings/class-wc-settings-page-test.php
+++ plugins/woocommerce/tests/php/includes/settings/class-wc-settings-page-test.php
@@ -342,9 +342,9 @@ HTML;
 	 * Test for add_settings_page_data.
 	 */
 	public function test_add_settings_page_data() {
-		$migration           = new WC_Settings_Migration_Test();
-		$setting_data        = $migration->add_settings_page_data( array() );
-		$migration_page_data = $setting_data[ $migration->get_id() ];
+		$migration                = new WC_Settings_Migration_Test();
+		$setting_data             = $migration->add_settings_page_data( array() );
+		$migration_page_data      = $setting_data[ $migration->get_id() ];
 		$migration_sections_data  = $migration_page_data['sections'];
 
 		print_r( $migration_sections_data['foobar']['settings'][0]['title'] );
