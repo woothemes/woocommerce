aa8a7b1863dfd121b54946852b7c98394449add5
Daniel W. Robert <danielwrobert@gmail.com>
2024-11-11 17:27:23 -0500

    Check to confirm as_has_scheduled_action exists
    Add a check to confirm that `as_has_scheduled_action` exists in the
    condition that uses it to conditionally run the warning log.

--- plugins/woocommerce/src/Blocks/BlockPatterns.php
+++ plugins/woocommerce/src/Blocks/BlockPatterns.php
@@ -155,7 +155,7 @@ class BlockPatterns {
 			// By only logging when patterns are empty and no fetch is scheduled,
 			// we ensure that warnings are only generated in genuinely problematic situations,
 			// such as when the pattern fetching mechanism has failed entirely.
-			if ( ! as_has_scheduled_action( 'fetch_patterns' ) ) {
+			if ( function_exists('as_has_scheduled_action') && ! as_has_scheduled_action( 'fetch_patterns' ) ) {
 				wc_get_logger()->warning(
 					__( 'Empty patterns received from the PTK Pattern Store', 'woocommerce' ),
 				);
