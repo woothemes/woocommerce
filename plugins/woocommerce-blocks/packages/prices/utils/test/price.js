1376cfb14473044f96830d152258c5eb11a52d8b
Tomek Wytrębowicz <tomalecpub@gmail.com>
2024-11-12 00:30:12 +0100

    Add LTR character to decode test
    Address https://github.com/woocommerce/woocommerce/pull/52676/files/885af50ca9246bbcd13ef586964399d37b97c0a2#diff-b3804b3092204ca0a05ac2b55aa2c770f02cabb65922f0172cb4305c237bbd46

--- plugins/woocommerce-blocks/packages/prices/utils/test/price.js
+++ plugins/woocommerce-blocks/packages/prices/utils/test/price.js
@@ -23,6 +23,7 @@ describe( 'The function formatPrice()', () => {
 		${ 10000000 }       | ${ '€ ' }      | ${ '' }  | ${ ',' }          | ${ '.' }         | ${ 0 }    | ${ '€ 10,000,000' }
 		${ 1000000099 }     | ${ '$' }       | ${ '' }  | ${ ',' }          | ${ '.' }         | ${ 2 }    | ${ '$10,000,000.99' }
 		${ 1000000099 }     | ${ '$' }       | ${ '' }  | ${ '.' }          | ${ ',' }         | ${ 2 }    | ${ '$10.000.000,99' }
+		${ 1000 }           | ${ '&cent;' }  | ${ '' }  | ${ '.' }          | ${ ',' }         | ${ 2 }    | ${ '¢10,00' }
 		${ 1000 }           | ${ '&#x62f;' } | ${ '' }  | ${ '.' }          | ${ ',' }         | ${ 2 }    | ${ 'د10,00' }
 	`(
 		'correctly formats price given "$value", "$prefix" prefix, "$suffix" suffix, "$thousandSeparator" thousandSeparator, "$decimalSeparator" decimalSeparator, and "$minorUnit" minorUnit as "$expected"',
