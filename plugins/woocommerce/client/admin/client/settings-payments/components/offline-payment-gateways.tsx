791d9936f8488eb4160e7451e4f2913088299bed
Oleksandr Aratovskyi <79862886+oaratovskyi@users.noreply.github.com>
2024-11-12 12:15:41 +0200

    NOX: Handle payment provider action items (#52576)
    * Add TODO
    
    * Remove embedded payment recommendations from settings page
    
    * Add changelog
    
    * Remove unused import in embedded-body-layout.tsx
    
    * WIP
    
    * Add WooPayments Gateway to preferred
    
    * Small walkaround to test WooPayments in JN site
    
    * Extract PaymentGatewayListItem into component, introduce types.ts
    
    * Extract PaymentExtensionSuggestionListItem into component
    
    * finish extracting into components
    
    * package.json change
    
    * Add notice about plugin install
    
    * Refactor SettingsPaymentsMain to introduce states in the main component (to pass them further to child components)
    
    * Fix to show WooPayments as preferred PE
    
    * - Extract offline payment gateway list item component
    - Rename vars
    - Make status update when PaymentGatewayButton is clicked
    
    * Remove changelog that is not needed
    
    * fix lint errors
    
    * fix lint errors 2
    
    * Roll back embedded payment recommendations - will remove in a separate PR
    
    * Use WooPayments asset URL for PGs (temporarily)
    
    ---------
    
    Co-authored-by: oaratovskyi <oleksandr.aratovskyi@automattic.com>

--- plugins/woocommerce/client/admin/client/settings-payments/components/offline-payment-gateways.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/offline-payment-gateways.tsx
@@ -1,33 +1,27 @@
 /**
  * External dependencies
  */
+import { List } from '@woocommerce/components';
+import { PaymentGateway } from '@woocommerce/data';
 import { Card, CardHeader } from '@wordpress/components';
 import React from '@wordpress/element';
-import { decodeEntities } from '@wordpress/html-entities';
 import { __ } from '@wordpress/i18n';
-import { List } from '@woocommerce/components';
-import { useSelect } from '@wordpress/data';
-import { PAYMENT_GATEWAYS_STORE_NAME } from '@woocommerce/data';
 
 /**
  * Internal dependencies
  */
 import { getAdminSetting } from '~/utils/admin-settings';
-import { PaymentGatewayButton } from './payment-gateway-button';
+import { OfflinePaymentGatewayListItem } from './offline-gateway-list-item';
 
 const assetUrl = getAdminSetting( 'wcAdminAssetUrl' );
 
-export const OfflinePaymentGateways = () => {
-	const { installedPaymentGateways } = useSelect( ( select ) => {
-		return {
-			getPaymentGateway: select( PAYMENT_GATEWAYS_STORE_NAME )
-				.getPaymentGateway,
-			installedPaymentGateways: select(
-				PAYMENT_GATEWAYS_STORE_NAME
-			).getPaymentGateways(),
-		};
-	}, [] );
+interface OfflinePaymentGatewaysProps {
+	registeredPaymentGateways: PaymentGateway[];
+}
 
+export const OfflinePaymentGateways = ( {
+	registeredPaymentGateways,
+}: OfflinePaymentGatewaysProps ) => {
 	// Mock payment gateways for now.
 	// TODO Get the list of gateways via the API in future PR.
 	const mockOfflinePaymentGateways = [
@@ -71,7 +65,7 @@ export const OfflinePaymentGateways = () => {
 
 	const availableOfflineGateways = mockOfflinePaymentGateways
 		.map( ( gateway ) => {
-			const installedGateway = installedPaymentGateways.find(
+			const installedGateway = registeredPaymentGateways.find(
 				( g ) => g.id === gateway.id
 			);
 
@@ -90,30 +84,9 @@ export const OfflinePaymentGateways = () => {
 	// Transform plugins comply with List component format.
 	const paymentGatewaysList = availableOfflineGateways.map( ( gateway ) => {
 		if ( ! gateway ) return null;
-		return {
-			key: gateway.id,
-			title: <>{ gateway.title }</>,
-			className: 'transitions-disabled',
-			content: <>{ decodeEntities( gateway.content ) }</>,
-			after: (
-				<PaymentGatewayButton
-					id={ gateway.id }
-					enabled={ gateway.enabled }
-					settings_url={ gateway.settings_url }
-				/>
-			),
-			// TODO add drag-and-drop icon before image (future PR)
-			before: (
-				<img
-					src={
-						gateway.square_image ||
-						gateway.image_72x72 ||
-						gateway.image
-					}
-					alt={ gateway.title + ' logo' }
-				/>
-			),
-		};
+		return OfflinePaymentGatewayListItem( {
+			gateway,
+		} );
 	} );
 
 	return (
---
+++ plugins/woocommerce/client/admin/client/settings-payments/components/payment-gateway-list-item/payment-gateway-list-item.tsx
@@ -0,0 +1,128 @@
+/**
+ * External dependencies
+ */
+import { useState } from 'react';
+import { EllipsisMenu } from '@woocommerce/components';
+import { PaymentGateway } from '@woocommerce/data';
+import { WooPaymentMethodsLogos } from '@woocommerce/onboarding';
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { decodeEntities } from '@wordpress/html-entities';
+
+/**
+ * Internal dependencies
+ */
+import { StatusBadge } from '~/settings-payments/components/status-badge';
+import { PaymentGatewayButton } from '~/settings-payments/components/payment-gateway-button';
+import { WooPaymentsGatewayData } from '~/settings-payments/types';
+import { WC_ASSET_URL } from '~/utils/admin-settings';
+
+type PaymentGatewayItemProps = {
+	gateway: PaymentGateway;
+	wooPaymentsGatewayData?: WooPaymentsGatewayData;
+	setupLivePayments: () => void;
+};
+
+export const PaymentGatewayListItem = ( {
+	gateway,
+	wooPaymentsGatewayData,
+	setupLivePayments,
+}: PaymentGatewayItemProps ) => {
+	const [ isEnabled, setIsEnabled ] = useState( gateway.enabled );
+
+	const isWCPay = [
+		'pre_install_woocommerce_payments_promotion',
+		'woocommerce_payments',
+	].includes( gateway.id );
+
+	const determineGatewayStatus = () => {
+		if ( ! isEnabled && gateway?.needs_setup ) {
+			return 'needs_setup';
+		}
+		if ( isEnabled ) {
+			if ( isWCPay ) {
+				if ( wooPaymentsGatewayData?.isInTestMode ) {
+					return 'test_mode';
+				}
+			}
+			return 'active';
+		}
+
+		if ( isWCPay ) {
+			return 'recommended';
+		}
+
+		return 'inactive';
+	};
+
+	return {
+		key: gateway.id,
+		className: isWCPay
+			? 'woocommerce-item__woocommerce-payment transitions-disabled'
+			: 'transitions-disabled',
+		title: (
+			<>
+				{ gateway.method_title }
+				<StatusBadge status={ determineGatewayStatus() } />
+			</>
+		),
+		content: (
+			<>
+				{ decodeEntities( gateway.method_description ) }
+				{ isWCPay && (
+					<WooPaymentMethodsLogos
+						maxElements={ 10 }
+						isWooPayEligible={ true }
+					/>
+				) }
+			</>
+		),
+		after: (
+			<div className="woocommerce-list__item-after__actions">
+				<>
+					<PaymentGatewayButton
+						id={ gateway.id }
+						enabled={ isEnabled }
+						needs_setup={ gateway.needs_setup }
+						settings_url={ gateway.settings_url }
+						setIsEnabled={ setIsEnabled }
+					/>
+					{ isWCPay && wooPaymentsGatewayData?.isInTestMode && (
+						<Button
+							variant="primary"
+							onClick={ setupLivePayments }
+							isBusy={ false }
+							disabled={ false }
+						>
+							{ __( 'Set up live payments', 'woocommerce' ) }
+						</Button>
+					) }
+					<EllipsisMenu
+						label={ __( 'Task List Options', 'woocommerce' ) }
+						renderContent={ () => (
+							<div>
+								<Button>
+									{ __( 'Learn more', 'woocommerce' ) }
+								</Button>
+								<Button>
+									{ __(
+										'See Terms of Service',
+										'woocommerce'
+									) }
+								</Button>
+							</div>
+						) }
+					/>
+				</>
+			</div>
+		),
+		// TODO add drag-and-drop icon before image (future PR)
+		before: (
+			<img
+				// TODO: Need a way to make images available here. For now it'll be a WooPayments image everywhere.
+				src={ `${ WC_ASSET_URL }images/onboarding/wcpay.svg` }
+				alt={ gateway.title + ' logo' }
+			/>
+		),
+	};
+};
