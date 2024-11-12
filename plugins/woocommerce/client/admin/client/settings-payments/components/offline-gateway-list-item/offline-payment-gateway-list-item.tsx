14251863e63c6cce238bb9bf03a29b135eaace35
Ilyas Foo <foo.ilyas@gmail.com>
2024-11-12 20:48:22 +0800

    Refactor payment settings list to use Sortable

--- plugins/woocommerce/client/admin/client/settings-payments/components/offline-gateway-list-item/offline-payment-gateway-list-item.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/offline-gateway-list-item/offline-payment-gateway-list-item.tsx
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import { useState } from 'react';
 import { decodeEntities } from '@wordpress/html-entities';
+import { SortableHandle } from '@woocommerce/components';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -24,39 +25,51 @@ interface OfflinePaymentGateway {
 
 type OfflinePaymentGatewayListItemProps = {
 	gateway: OfflinePaymentGateway;
+	index: number;
 };
 
 export const OfflinePaymentGatewayListItem = ( {
 	gateway,
+	index,
 }: OfflinePaymentGatewayListItemProps ) => {
 	const [ isEnabled, setIsEnabled ] = useState( gateway.enabled );
 
-	return {
-		key: gateway.id,
-		title: <>{ gateway.title }</>,
-		className: 'transitions-disabled',
-		content: (
-			<span
-				dangerouslySetInnerHTML={ sanitizeHTML(
-					decodeEntities( gateway.content )
-				) }
-			/>
-		),
-		after: (
-			<PaymentGatewayButton
-				id={ gateway.id }
-				enabled={ isEnabled }
-				settings_url={ gateway.settings_url }
-				setIsEnabled={ setIsEnabled }
-			/>
-		),
-		before: (
-			<img
-				src={
-					gateway.square_image || gateway.image_72x72 || gateway.image
-				}
-				alt={ gateway.title + ' logo' }
-			/>
-		),
-	};
+	return (
+		<div className="woocommerce-list__item woocommerce-list__item-enter-done">
+			<div className="woocommerce-list__item-inner">
+				<div className="woocommerce-list__item-before">
+					<SortableHandle itemIndex={ index } />
+					<img
+						src={
+							gateway.square_image ||
+							gateway.image_72x72 ||
+							gateway.image
+						}
+						alt={ gateway.title + ' logo' }
+					/>
+				</div>
+				<div className="woocommerce-list__item-text">
+					<span className="woocommerce-list__item-title">
+						{ gateway.title }
+					</span>
+					<span
+						className="woocommerce-list__item-content"
+						dangerouslySetInnerHTML={ sanitizeHTML(
+							decodeEntities( gateway.content )
+						) }
+					/>
+				</div>
+				<div className="woocommerce-list__item-after">
+					<div className="woocommerce-list__item-after__actions">
+						<PaymentGatewayButton
+							id={ gateway.id }
+							enabled={ isEnabled }
+							settings_url={ gateway.settings_url }
+							setIsEnabled={ setIsEnabled }
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
