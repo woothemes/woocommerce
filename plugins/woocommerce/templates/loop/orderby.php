f080ecdc49c322179a578458523392ad30606dab
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-11-14 11:30:47 -0300

    Add visual label to orderby select box

--- plugins/woocommerce/templates/loop/orderby.php
+++ plugins/woocommerce/templates/loop/orderby.php
@@ -21,7 +21,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
+	<label for="woocommerce-orderby"><?php echo esc_html( 'Sort by', 'woocommerce' ); ?></label>
+	<select id="woocommerce-orderby" name="orderby" class="orderby">
 		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
 			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
 		<?php endforeach; ?>
