a77916ff21394540edc280ddb877f08dff220833
Ilyas Foo <foo.ilyas@gmail.com>
2024-11-14 18:18:22 +0800

    Use proper sortable

--- plugins/woocommerce/client/admin/client/settings-payments/settings-payments-offline.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/settings-payments-offline.tsx
@@ -4,22 +4,28 @@
 import '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { PAYMENT_GATEWAYS_STORE_NAME } from '@woocommerce/data';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
 import './settings-payments-offline.scss';
 import { OfflinePaymentGateways } from './components/offline-payment-gateways';
+import usePaymentOrdering from './hooks/use-payment-ordering';
 
 export const SettingsPaymentsOffline = () => {
 	const registeredPaymentGateways = useSelect( ( select ) => {
 		return select( PAYMENT_GATEWAYS_STORE_NAME ).getPaymentGateways();
 	}, [] );
 
+	const { ordering, updateOrdering } = usePaymentOrdering( 'offline' );
+
 	return (
 		<div className="settings-payments-offline__container">
 			<OfflinePaymentGateways
 				registeredPaymentGateways={ registeredPaymentGateways }
+				ordering={ ordering }
+				updateOrdering={ updateOrdering }
 			/>
 		</div>
 	);
--- plugins/woocommerce/client/admin/client/settings-payments/utils.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/utils.ts
@@ -2,3 +2,17 @@ export const parseScriptTag = ( elementId: string ) => {
 	const scriptTag = document.getElementById( elementId );
 	return scriptTag ? JSON.parse( scriptTag.textContent || '' ) : [];
 };
+
+/**
+ * Parse an array of items into an ordering object.
+ *
+ * @param items - The items to parse.
+ * @return The ordering object.
+ */
+export const parseOrdering = < T extends { id: string | number } >(
+	items: T[]
+) => {
+	return items.reduce( ( acc, item, index ) => {
+		return { ...acc, [ item.id ]: index };
+	}, {} );
+};
