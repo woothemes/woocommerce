f3c4ff8ca2057c2aeebea7fe58f01f1eac8111bf
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-11-14 23:18:05 +0800

    Fix wc blocks tests

--- plugins/woocommerce-blocks/tests/js/setup-after-env.ts
+++ plugins/woocommerce-blocks/tests/js/setup-after-env.ts
@@ -3,3 +3,17 @@
  */
 import '@testing-library/jest-dom';
 import '@wordpress/jest-console';
+
+const consoleErrorSpy = jest
+	.spyOn( console, 'error' )
+	.mockImplementation( ( message, ...args ) => {
+		// Ignore messages that match the pattern "Store ".*" is already registered." because we have duplicate registration in the test environment as there are multiple instances of the same store such as core-data, core/block-editor, etc.
+		if (
+			typeof message === 'string' &&
+			message.match( /Store ".*" is already registered/ )
+		) {
+			return;
+		}
+		// Otherwise, call the original console.error
+		consoleErrorSpy.mock.calls.push( [ message, ...args ] );
+	} );
