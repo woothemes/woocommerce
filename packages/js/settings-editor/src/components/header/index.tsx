c05df1ffe2b212f8ce12470fa7851de366c20392
paul sealock <psealock@gmail.com>
2024-11-15 14:25:44 +1300

    use header

--- packages/js/settings-editor/src/components/header/index.tsx
+++ packages/js/settings-editor/src/components/header/index.tsx
@@ -2,9 +2,32 @@
  * External dependencies
  */
 import { createElement } from '@wordpress/element';
+import {
+	// @ts-expect-error missing types.
+	__experimentalHeading as Heading,
+	// @ts-expect-error missing types.
+	__experimentalText as Text,
+	// @ts-expect-error missing types.
+	__experimentalHStack as HStack,
+	// @ts-expect-error missing types.
+	__experimentalVStack as VStack,
+	FlexItem,
+} from '@wordpress/components';
 
 export const Header = ( { pageTitle }: { pageTitle: string } ) => {
 	return (
-		<div className="woocommerce-settings-header">Header: { pageTitle }</div>
+		<VStack className="edit-site-page-header" as="header" spacing={ 0 }>
+			<HStack className="edit-site-page-header__page-title">
+				<Heading
+					as="h2"
+					level={ 3 }
+					weight={ 500 }
+					className="edit-site-page-header__title"
+					truncate
+				>
+					{ pageTitle }
+				</Heading>
+			</HStack>
+		</VStack>
 	);
 };
