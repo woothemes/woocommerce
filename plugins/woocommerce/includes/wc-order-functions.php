d8480cedc2f0bbceae6be6efa69bbf59531a8ed1
barryhughes <3594411+barryhughes@users.noreply.github.com>
2024-11-15 15:53:05 -0800

    Use status constants, and ensure we test the type of the status as originally provided.

--- plugins/woocommerce/includes/wc-order-functions.php
+++ plugins/woocommerce/includes/wc-order-functions.php
@@ -8,6 +8,7 @@
  * @version 3.4.0
  */
 
+use Automattic\WooCommerce\Enums\OrderStatus;
 use Automattic\WooCommerce\Internal\DataStores\Orders\DataSynchronizer;
 use Automattic\WooCommerce\Internal\Utilities\Users;
 use Automattic\WooCommerce\Utilities\OrderUtil;
@@ -150,16 +151,16 @@ function wc_get_order_status_name( $status ) {
 	// "Special statuses": these are in common usage across WooCommerce, but are not normally returned by
 	// wc_get_order_statuses(). We add them only to avoid unnecessary doing-it-wrong errors.
 	$special_statuses = array(
-		'wc-auto-draft' => 'auto-draft',
-		'wc-trash'      => 'trash',
+		'wc-' . OrderStatus::AUTO_DRAFT => OrderStatus::AUTO_DRAFT,
+		'wc-' . OrderStatus::TRASH      => OrderStatus::TRASH,
 	);
 
 	// Merge order is important. If the special statuses are ever returned by wc_get_order_statuses(), those definitions
 	// should take priority.
-	$statuses = array_merge( $special_statuses, wc_get_order_statuses() );
-	$status   = OrderUtil::remove_status_prefix( (string) $status );
+	$statuses   = array_merge( $special_statuses, wc_get_order_statuses() );
+	$unprefixed = OrderUtil::remove_status_prefix( (string) $status );
 
-	if ( ! is_string( $status ) || ! isset( $statuses[ 'wc-' . $status ] ) ) {
+	if ( ! is_string( $status ) || ! isset( $statuses[ 'wc-' . $unprefixed ] ) ) {
 		wc_doing_it_wrong(
 			__FUNCTION__,
 			__( 'An invalid order status slug was supplied.', 'woocommerce' ),
@@ -167,7 +168,7 @@ function wc_get_order_status_name( $status ) {
 		);
 	}
 
-	return $statuses[ 'wc-' . $status ] ?? $status;
+	return $statuses[ 'wc-' . $unprefixed ] ?? $unprefixed;
 }
 
 /**
