1297a33097fa8454cd8efd85c22633c0b2109670
 Ján Mikláš <neosinner@gmail.com>
2024-11-20 11:08:17 +0100

    Don't fire change event when changing type in email preview

--- plugins/woocommerce/client/admin/client/settings-email/settings-email-preview-type.tsx
+++ plugins/woocommerce/client/admin/client/settings-email/settings-email-preview-type.tsx
@@ -16,7 +16,7 @@ export const EmailPreviewType: React.FC< EmailPreviewTypeProps > = ( {
 	setEmailType,
 } ) => {
 	return (
-		<div className="wc-settings-email-preview-type">
+		<div className="wc-settings-email-preview-type wc-settings-prevent-change-event">
 			<SelectControl
 				onChange={ setEmailType }
 				options={ emailTypes }
