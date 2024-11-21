bbc57d5cda0662bc255b4abe270fd667ca58a3cb
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-11-21 15:57:08 +0100

    Refactor and simplify tests

--- plugins/woocommerce-blocks/tests/e2e/tests/patterns/add-new-pattern.classic_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/patterns/add-new-pattern.classic_theme.spec.ts
@@ -6,6 +6,7 @@ import { test, expect, CLASSIC_THEME_SLUG } from '@woocommerce/e2e-utils';
 /**
  * Internal dependencies
  */
+import { addTestingBlocks, expectedTitles, expectedPrices } from './utils';
 
 test.describe( 'Patterns in classic theme', () => {
 	test.beforeEach( async ( { requestUtils } ) => {
@@ -18,42 +19,12 @@ test.describe( 'Patterns in classic theme', () => {
 	} ) => {
 		await admin.createNewPattern( 'Woo Blocks Unsynced Pattern' );
 
-		// Add testing blocks
-		await editor.insertBlockUsingGlobalInserter( 'On Sale Products' );
-		await editor.insertBlockUsingGlobalInserter( 'Single Product' );
-		await editor.canvas.getByText( 'Album' ).click();
-		await editor.canvas.getByText( 'Done' ).click();
+		const { productTitles, productPrices } = await addTestingBlocks(
+			editor
+		);
 
-		// Product Collection optimizes rendering products so 2nd+ products
-		// are not accessible through block selectors like getByLabel( 'Block: Product Title' )
-		// hence CSS selectors and condition to be visible.
-		const productTitles = editor.canvas
-			.locator( '.wp-block-post-title' )
-			.locator( 'visible=true' );
-		const productPrices = editor.canvas
-			.getByLabel( 'Block: Product Price' )
-			.locator( 'visible=true' );
-
-		await expect( productTitles ).toHaveText( [
-			// Product Collection
-			'Beanie',
-			'Beanie with Logo',
-			'Belt',
-			'Cap',
-			'Hoodie',
-			// Single Product
-			'Album',
-		] );
-		await expect( productPrices ).toHaveText( [
-			// Product Collection
-			'Previous price:$20.00Discounted price:$18.00',
-			'Previous price:$20.00Discounted price:$18.00',
-			'Previous price:$65.00Discounted price:$55.00',
-			'Previous price:$18.00Discounted price:$16.00',
-			'Previous price:$45.00Discounted price:$42.00',
-			// Single Product
-			'$15.00',
-		] );
+		await expect( productTitles ).toHaveText( expectedTitles );
+		await expect( productPrices ).toHaveText( expectedPrices );
 	} );
 
 	test( 'Unsynced Pattern can be created with basic blocks', async ( {
@@ -62,41 +33,11 @@ test.describe( 'Patterns in classic theme', () => {
 	} ) => {
 		await admin.createNewPattern( 'Woo Blocks Unsynced Pattern', false );
 
-		// Add testing blocks
-		await editor.insertBlockUsingGlobalInserter( 'On Sale Products' );
-		await editor.insertBlockUsingGlobalInserter( 'Single Product' );
-		await editor.canvas.getByText( 'Album' ).click();
-		await editor.canvas.getByText( 'Done' ).click();
+		const { productTitles, productPrices } = await addTestingBlocks(
+			editor
+		);
 
-		// Product Collection optimizes rendering products so 2nd+ products
-		// are not accessible through block selectors like getByLabel( 'Block: Product Title' )
-		// hence CSS selectors and condition to be visible.
-		const productTitles = editor.canvas
-			.locator( '.wp-block-post-title' )
-			.locator( 'visible=true' );
-		const productPrices = editor.canvas
-			.getByLabel( 'Block: Product Price' )
-			.locator( 'visible=true' );
-
-		await expect( productTitles ).toHaveText( [
-			// Product Collection
-			'Beanie',
-			'Beanie with Logo',
-			'Belt',
-			'Cap',
-			'Hoodie',
-			// Single Product
-			'Album',
-		] );
-		await expect( productPrices ).toHaveText( [
-			// Product Collection
-			'Previous price:$20.00Discounted price:$18.00',
-			'Previous price:$20.00Discounted price:$18.00',
-			'Previous price:$65.00Discounted price:$55.00',
-			'Previous price:$18.00Discounted price:$16.00',
-			'Previous price:$45.00Discounted price:$42.00',
-			// Single Product
-			'$15.00',
-		] );
+		await expect( productTitles ).toHaveText( expectedTitles );
+		await expect( productPrices ).toHaveText( expectedPrices );
 	} );
 } );
