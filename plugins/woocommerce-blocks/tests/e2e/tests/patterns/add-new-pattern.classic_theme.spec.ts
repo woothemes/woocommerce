38272c103559127653ae2355758529acf3934453
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-11-21 15:44:31 +0100

    Add E2E tests for classic theme

---
+++ plugins/woocommerce-blocks/tests/e2e/tests/patterns/add-new-pattern.classic_theme.spec.ts
@@ -0,0 +1,102 @@
+/**
+ * External dependencies
+ */
+import { test, expect, CLASSIC_THEME_SLUG } from '@woocommerce/e2e-utils';
+
+/**
+ * Internal dependencies
+ */
+
+test.describe( 'Patterns in classic theme', () => {
+	test.beforeEach( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( CLASSIC_THEME_SLUG );
+	} );
+
+	test( 'Synced Pattern can be created with basic blocks', async ( {
+		admin,
+		editor,
+	} ) => {
+		await admin.createNewPattern( 'Woo Blocks Unsynced Pattern' );
+
+		// Add testing blocks
+		await editor.insertBlockUsingGlobalInserter( 'On Sale Products' );
+		await editor.insertBlockUsingGlobalInserter( 'Single Product' );
+		await editor.canvas.getByText( 'Album' ).click();
+		await editor.canvas.getByText( 'Done' ).click();
+
+		// Product Collection optimizes rendering products so 2nd+ products
+		// are not accessible through block selectors like getByLabel( 'Block: Product Title' )
+		// hence CSS selectors and condition to be visible.
+		const productTitles = editor.canvas
+			.locator( '.wp-block-post-title' )
+			.locator( 'visible=true' );
+		const productPrices = editor.canvas
+			.getByLabel( 'Block: Product Price' )
+			.locator( 'visible=true' );
+
+		await expect( productTitles ).toHaveText( [
+			// Product Collection
+			'Beanie',
+			'Beanie with Logo',
+			'Belt',
+			'Cap',
+			'Hoodie',
+			// Single Product
+			'Album',
+		] );
+		await expect( productPrices ).toHaveText( [
+			// Product Collection
+			'Previous price:$20.00Discounted price:$18.00',
+			'Previous price:$20.00Discounted price:$18.00',
+			'Previous price:$65.00Discounted price:$55.00',
+			'Previous price:$18.00Discounted price:$16.00',
+			'Previous price:$45.00Discounted price:$42.00',
+			// Single Product
+			'$15.00',
+		] );
+	} );
+
+	test( 'Unsynced Pattern can be created with basic blocks', async ( {
+		admin,
+		editor,
+	} ) => {
+		await admin.createNewPattern( 'Woo Blocks Unsynced Pattern', false );
+
+		// Add testing blocks
+		await editor.insertBlockUsingGlobalInserter( 'On Sale Products' );
+		await editor.insertBlockUsingGlobalInserter( 'Single Product' );
+		await editor.canvas.getByText( 'Album' ).click();
+		await editor.canvas.getByText( 'Done' ).click();
+
+		// Product Collection optimizes rendering products so 2nd+ products
+		// are not accessible through block selectors like getByLabel( 'Block: Product Title' )
+		// hence CSS selectors and condition to be visible.
+		const productTitles = editor.canvas
+			.locator( '.wp-block-post-title' )
+			.locator( 'visible=true' );
+		const productPrices = editor.canvas
+			.getByLabel( 'Block: Product Price' )
+			.locator( 'visible=true' );
+
+		await expect( productTitles ).toHaveText( [
+			// Product Collection
+			'Beanie',
+			'Beanie with Logo',
+			'Belt',
+			'Cap',
+			'Hoodie',
+			// Single Product
+			'Album',
+		] );
+		await expect( productPrices ).toHaveText( [
+			// Product Collection
+			'Previous price:$20.00Discounted price:$18.00',
+			'Previous price:$20.00Discounted price:$18.00',
+			'Previous price:$65.00Discounted price:$55.00',
+			'Previous price:$18.00Discounted price:$16.00',
+			'Previous price:$45.00Discounted price:$42.00',
+			// Single Product
+			'$15.00',
+		] );
+	} );
+} );
