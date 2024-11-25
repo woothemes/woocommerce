9071d081dfe753615780eec18284c19023d255c9
Wesley Rosa <wesleyjrosa@gmail.com>
2024-11-25 09:18:21 -0300

    Revert filter invocation frequency changes

--- plugins/woocommerce/includes/data-stores/class-wc-order-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-order-data-store-cpt.php
@@ -185,18 +185,20 @@ class WC_Order_Data_Store_CPT extends Abstract_WC_Order_Data_Store_CPT implement
 	 * @param WC_Order $order Order object.
 	 */
 	public function update( &$order ) {
-		/**
-		 * Filter the order status to use when payment is complete.
-		 *
-		 * @since 3.0.0
-		 *
-		 * @param string   $payment_complete_status Default status to use when payment is complete.
-		 * @param int      $order_id               Order ID.
-		 */
-		$payment_complete_status = apply_filters( 'woocommerce_payment_complete_order_status', $order->needs_processing() ? OrderStatus::PROCESSING : OrderStatus::COMPLETED, $order->get_id(), $order );
 		// Before updating, ensure date paid is set if missing.
-		if ( ! $order->get_date_paid( 'edit' ) && version_compare( $order->get_version( 'edit' ), '3.0', '<' ) && $order->has_status( $payment_complete_status ) ) {
-			$order->set_date_paid( $order->get_date_created( 'edit' ) );
+		if ( ! $order->get_date_paid( 'edit' ) && version_compare( $order->get_version( 'edit' ), '3.0', '<' ) ) {
+			/**
+			 * Filter the order status to use when payment is complete.
+			 *
+			 * @since 3.0.0
+			 *
+			 * @param string   $payment_complete_status Default status to use when payment is complete.
+			 * @param int      $order_id               Order ID.
+			 */
+			$payment_complete_status = apply_filters( 'woocommerce_payment_complete_order_status', $order->needs_processing() ? OrderStatus::PROCESSING : OrderStatus::COMPLETED, $order->get_id(), $order );
+			if ( $order->has_status( $payment_complete_status ) ) {
+				$order->set_date_paid( $order->get_date_created( 'edit' ) );
+			}
 		}
 
 		// Also grab the current status so we can compare.
