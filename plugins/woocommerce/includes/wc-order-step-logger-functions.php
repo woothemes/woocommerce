137a88e06c2cda7339f0a8df1883ad13766ad941
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-11-28 00:35:11 +0000

    Refactored for performance, avoided polluting the global namespace, and introduced saving by trace uid

--- plugins/woocommerce/includes/wc-order-step-logger-functions.php
+++ plugins/woocommerce/includes/wc-order-step-logger-functions.php
@@ -14,39 +14,37 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Log an order-related message. This is not public API and should not be used by plugins or themes.
  *
- * @param string $message Message to log.
- * @param array $context Optional. Additional information for log handlers.
+ * @param string     $message Message to log.
+ * @param array|null $context Optional. Additional information for log handlers.
  *
  * @internal This function is intended for internal use only.
+ * @since 9.6.0
  */
-function wc_log_order_step( string $message, array $context = array() ) {
+function wc_log_order_step( string $message, ?array $context = null ) {
 
-	if ( empty( $message ) ) {
+	if ( '' === $message ) {
 		return; // Nothing to log.
 	}
 
-	// Generate a place order unique ID for logging purposes. If this is called multiple times,
+	static $order_uid = null;
+	static $logger = null;
+
+	// Generate a static place order unique ID for logging purposes. When this is called multiple times,
 	// the same UID will be used, enabling us to track recursion and race-condition issues on order processing methods
-	// or other problems related to third-party plugins.
-	if ( ! defined( 'WC_ORDER_STEP_LOGGER_UID' ) ) {
-		define( 'WC_ORDER_STEP_LOGGER_UID', wp_generate_uuid4() );
+	// or other problems related to third-party plugins and filters.
+	if ( ! $order_uid ) {
+		$order_uid = wp_generate_uuid4();
 	}
 
-	// Without a string place order request's unique ID to add to the log context, bail.
-	if ( ! is_string( WC_ORDER_STEP_LOGGER_UID ) ) {
-		return;
+	$context              = $context ?? [];
+	$context['order_uid'] = $order_uid;
+	$context['source']    = 'place-order-debug-' . substr( $order_uid, 0, 8 );
+
+	if ( ! $logger ) {
+		// Use a static logger instance to avoid unnecessary instantiations.
+		$logger = new WC_Logger();
 	}
 
-	$context['order_uid'] = WC_ORDER_STEP_LOGGER_UID;
-	$context['source']    = 'checkout-step-debug';
-
-	global $wc_log_order_logger;
-
-	if ( ! isset( $wc_log_order_logger ) ) {
-		// Instantiate and store the logger instance in a global to avoid creating multiple instances.
-		$wc_log_order_logger = new WC_Logger();
-	}
-
-	// Logging the place order flow step completed. Logs are grouped per day to make is easier to navigate.
-	$wc_log_order_logger->info( $message, $context );
+	// Logging the place order flow step completed. Log files are grouped per order to make is easier to navigate.
+	$logger->log( WC_Log_Levels::INFO, $message, $context );
 }
