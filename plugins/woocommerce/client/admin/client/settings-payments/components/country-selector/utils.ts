8df5cc5582e0d1929f1a95656b163db8046ec9d9
mordeth <cvetan.cvetanov@automattic.com>
2024-11-28 21:55:33 +0200

    Replace truncate method with text-overflow

--- plugins/woocommerce/client/admin/client/settings-payments/components/country-selector/utils.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/components/country-selector/utils.ts
@@ -3,20 +3,9 @@
  */
 import { Item } from './types';
 
-// Truncate the option label if it exceeds the max length.
-export const truncateOptionlabel = ( label: string, maxLength: number ) => {
-	if ( label.length > maxLength ) {
-		return `${ label.substring( 0, maxLength ) }...`;
-	}
-
-	return label;
-};
-
 // Get the label for the selected option.
 export const getOptionLabel = ( value: string, options: Item[] ) => {
 	const item = options.find( ( option ) => option.key === value );
 
-	const label = item?.name ? item.name : '';
-
-	return truncateOptionlabel( label, 20 );
+	return item?.name ? item.name : '';
 };
