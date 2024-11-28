480ca8547ebd7b5ed1820e8fab1d52be5ae941c5
Sam Seay <samueljseay@gmail.com>
2024-11-28 17:32:41 +0800

    Finish up adding heading and footer heading to checkout order summary attrs

--- plugins/woocommerce/src/Blocks/BlockTypes/CheckoutOrderSummaryBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/CheckoutOrderSummaryBlock.php
@@ -12,6 +12,9 @@ class CheckoutOrderSummaryBlock extends AbstractInnerBlock {
 	 */
 	protected $block_name = 'checkout-order-summary-block';
 
+	const HEADING_SETTING        = 'woocommerce_order_summary_heading';
+	const FOOTER_HEADING_SETTING = 'woocommerce_order_summary_footer_heading';
+
 	/**
 	 * Get the contents of the given inner block.
 	 *
@@ -47,8 +50,24 @@ class CheckoutOrderSummaryBlock extends AbstractInnerBlock {
 	protected function render( $attributes, $content, $block ) {
 		// The order-summary-totals block was introduced as a new parent block for the totals
 		// (subtotal, discount, fees, shipping and taxes) blocks.
+
+		$heading        = $this->get_heading_option();
+		$footer_heading = $this->get_footer_heading_option();
+
+		$extra_attributes = array();
+
+		if ( $heading ) {
+			$extra_attributes['heading'] = $heading;
+		}
+
+		if ( $footer_heading ) {
+			$extra_attributes['footerHeading'] = $footer_heading;
+		}
+
+		$attributes = get_block_wrapper_attributes( $extra_attributes );
+
 		$regex_for_checkout_order_summary_totals = '/<div data-block-name="woocommerce\/checkout-order-summary-totals-block"(.+?)>/';
-		$order_summary_totals_content            = '<div data-block-name="woocommerce/checkout-order-summary-totals-block" class="wp-block-woocommerce-checkout-order-summary-totals-block">';
+		$order_summary_totals_content            = sprintf( '<div %s>', $attributes );
 
 		// We want to move these blocks inside a parent 'totals' block.
 		$totals_inner_blocks = array( 'subtotal', 'discount', 'fee', 'shipping', 'taxes' );
@@ -76,5 +95,54 @@ class CheckoutOrderSummaryBlock extends AbstractInnerBlock {
 
 		// Remove empty lines.
 		return preg_replace( '/\n\n( *?)/i', '', $content );
+	}
+
+	/**
+	 * Get the heading option name.
+	 *
+	 * @return string
+	 */
+	protected function get_heading_option() {
+		return get_option( self::HEADING_SETTING, null );
+	}
+
+	/**
+	 * Get the footer heading option name.
+	 *
+	 * @return string
+	 */
+	protected function get_footer_heading_option() {
+		return get_option( self::FOOTER_HEADING_SETTING, null );
+	}
+
+	/**
+	 * Register the settings for the order summary heading and footer heading.
+	 */
+	public function register_settings() {
+		register_setting(
+			'woocommerce_order_summary',
+			self::HEADING_SETTING,
+			array(
+				'type'              => 'string',
+				'label'             => __( 'Order summary heading', 'woocommerce' ),
+				'description'       => __( 'Heading for the order summary section.', 'woocommerce' ),
+				'sanitize_callback' => 'sanitize_text_field',
+				'default'           => null,
+				'show_in_rest'      => true,
+			)
+		);
+
+		register_setting(
+			'woocommerce_order_summary',
+			self::FOOTER_HEADING_SETTING,
+			array(
+				'type'              => 'string',
+				'label'             => __( 'Order summary footer heading', 'woocommerce' ),
+				'description'       => __( 'Heading for the order summary footer section.', 'woocommerce' ),
+				'sanitize_callback' => 'sanitize_text_field',
+				'default'           => null,
+				'show_in_rest'      => true,
+			)
+		);
 	}
 }
