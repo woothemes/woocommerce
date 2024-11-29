38435ee6a54411a2a4be91e70e7772ffc399eca9
Thomas Roberts <thomas.roberts@automattic.com>
2024-11-29 16:03:56 +0000

    If a checkbox is required, render the ValidatedCheckboxControl component

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/form/form.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/form/form.tsx
@@ -5,6 +5,7 @@ import {
 	ValidatedTextInput,
 	type ValidatedTextInputHandle,
 	CheckboxControl,
+	ValidatedCheckboxControl,
 } from '@woocommerce/blocks-components';
 import {
 	BillingCountryInput,
@@ -195,19 +196,29 @@ const Form = < T extends AddressFormValues | ContactFormValues >( {
 				}
 
 				if ( field.type === 'checkbox' ) {
+					const checkboxProps = {
+						checked: Boolean( values[ field.key as keyof T ] ),
+						onChange: ( checked: boolean ) => {
+							onChange( {
+								...values,
+								[ field.key ]: checked,
+							} );
+						},
+						...checkboxFieldProps,
+					};
+					if ( field.required ) {
+						return (
+							<ValidatedCheckboxControl
+								key={ field.key }
+								{ ...checkboxProps }
+							/>
+						);
+					}
+
 					return (
 						<CheckboxControl
 							key={ field.key }
-							checked={ Boolean(
-								values[ field.key as keyof T ]
-							) }
-							onChange={ ( checked: boolean ) => {
-								onChange( {
-									...values,
-									[ field.key ]: checked,
-								} );
-							} }
-							{ ...checkboxFieldProps }
+							{ ...checkboxProps }
 						/>
 					);
 				}
