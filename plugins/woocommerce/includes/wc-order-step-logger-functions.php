d702d523c99a7821212a56fb86e9140a482e702b
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-11-29 01:46:51 +0000

    Performance tweak.

--- plugins/woocommerce/includes/wc-order-step-logger-functions.php
+++ plugins/woocommerce/includes/wc-order-step-logger-functions.php
@@ -26,20 +26,22 @@ function wc_log_order_step( string $message, ?array $context = null, bool $final
 		return; // Nothing to log.
 	}
 
-	static $steps     = array(); // Static array to store the messages and validate against unique messages before clearing the log.
-	static $order_uid = null;
-	static $logger    = null;
+	static $steps          = array(); // Static array to store the messages and validate against unique messages before clearing the log.
+	static $logger         = null;
+	static $order_uid      = null;
+	static $order_ui_short = null;
 
 	// Generate a static place order unique ID for logging purposes. When this is called multiple times in the same request,
 	// the same UID will be used, enabling us to track recursion and race-condition issues on order processing methods
 	// or other problems related to third-party plugins and filters.
 	if ( ! $order_uid ) {
-		$order_uid = wp_generate_uuid4();
+		$order_uid       = wp_generate_uuid4();
+		$order_uid_short = substr( $order_uid, 0, 8 );
 	}
 
 	$context['order_uid'] = $order_uid;
 	// Source is segmented per order unique id.
-	$context['source'] = 'place-order-debug-' . substr( $order_uid, 0, 8 );
+	$context['source'] = 'place-order-debug-' . $order_uid_short;
 
 	if ( ! $logger ) {
 		// Use a static logger instance to avoid unnecessary instantiations.
