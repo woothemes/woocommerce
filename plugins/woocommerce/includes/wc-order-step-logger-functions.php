47130ff7fa3fe7d1bbdaf19aa3e80b8330976051
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-11-29 01:57:45 +0000

    Improved var declaration.

--- plugins/woocommerce/includes/wc-order-step-logger-functions.php
+++ plugins/woocommerce/includes/wc-order-step-logger-functions.php
@@ -26,10 +26,8 @@ function wc_log_order_step( string $message, ?array $context = null, bool $final
 		return; // Nothing to log.
 	}
 
-	static $steps           = array(); // Static array to store the messages and validate against unique messages before clearing the log.
-	static $logger          = null;
-	static $order_uid       = null;
-	static $order_uid_short = null;
+	static $logger, $order_uid, $order_uid_short;
+	static $steps = array(); // Static array to store the messages and validate against unique messages before clearing the log.
 
 	// Generate a static place order unique ID for logging purposes. When this is called multiple times in the same request,
 	// the same UID will be used, enabling us to track recursion and race-condition issues on order processing methods
