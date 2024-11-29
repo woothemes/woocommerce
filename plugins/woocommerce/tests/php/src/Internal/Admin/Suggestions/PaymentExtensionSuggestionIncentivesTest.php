b0f6c2e18784e219ea4eccd709d6dee1f0144642
Vlad Olaru <vlad@pixelgrade.com>
2024-11-29 18:40:58 +0200

    test: Add unit tests for payment extension suggestion incentives provider class

---
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Suggestions/PaymentExtensionSuggestionIncentivesTest.php
@@ -0,0 +1,490 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Tests\Internal\Admin\Suggestions;
+
+use Automattic\WooCommerce\Internal\Admin\Suggestions\PaymentExtensionSuggestionIncentives;
+use Automattic\WooCommerce\Tests\Internal\Admin\Settings\Mocks\FakeIncentive;
+use PHPUnit\Framework\MockObject\MockObject;
+use WC_REST_Unit_Test_Case;
+
+/**
+ * PaymentExtensionSuggestionIncentives provider test.
+ *
+ * @class PaymentExtensionSuggestionIncentives
+ */
+class PaymentExtensionSuggestionIncentivesTest extends WC_REST_Unit_Test_Case {
+	/**
+	 * @var PaymentExtensionSuggestionIncentives
+	 */
+	protected $sut;
+
+	/**
+	 * @var FakeIncentive|MockObject
+	 */
+	protected $mock_incentive_provider;
+
+	/**
+	 * Set up test.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->mock_incentive_provider = $this->getMockBuilder( FakeIncentive::class )
+			->disableOriginalConstructor()
+			->onlyMethods( array( 'get_all', 'is_visible', 'is_dismissed' ) )
+			->getMock();
+
+		// Mock the get_incentive_instance method.
+		$this->sut = $this->getMockBuilder( PaymentExtensionSuggestionIncentives::class )
+			->onlyMethods( array( 'get_incentive_instance' ) )
+			->getMock();
+	}
+
+	public function test_get_incentive_no_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( null );
+
+		// Act.
+		$incentive = $this->sut->get_incentive( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertNull( $incentive );
+	}
+
+	public function test_get_incentive_no_incentive() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO' )
+			->willReturn( array() );
+
+		// Act.
+		$incentive = $this->sut->get_incentive( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertNull( $incentive );
+	}
+
+	public function test_get_incentive() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive1',
+					'type' => 'type1',
+				)
+			) );
+
+		// All are visible.
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'is_visible' )
+			->willReturn( true );
+
+		// Act.
+		$incentive = $this->sut->get_incentive( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertEquals( 'incentive1', $incentive['id'] );
+	}
+
+	public function test_get_incentive_with_type() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO', 'type1' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive1',
+					'type' => 'type1',
+				)
+			) );
+
+		// All are visible.
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'is_visible' )
+			->willReturn( true );
+
+		// Act.
+		$incentive = $this->sut->get_incentive( 'suggestion1', 'RO', 'type1' );
+
+		// Assert.
+		$this->assertEquals( 'incentive1', $incentive['id'] );
+	}
+
+	public function test_get_incentive_with_type_no_incentive() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO', 'bogus_type' )
+			->willReturn( array() );
+
+		// Act.
+		$incentive = $this->sut->get_incentive( 'suggestion1', 'RO', 'bogus_type' );
+
+		// Assert.
+		$this->assertNull( $incentive );
+	}
+
+	public function test_get_incentives_no_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( null );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertSame( array(), $incentives );
+	}
+
+	public function test_get_incentives_no_incentives() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO' )
+			->willReturn( array() );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertSame( array(), $incentives );
+	}
+
+	public function test_get_incentives() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive1',
+					'type' => 'type1',
+				),
+				array(
+					'id'   => 'incentive2',
+					'type' => 'type2',
+				),
+			) );
+
+		// All are visible.
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'is_visible' )
+			->willReturn( true );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertCount( 2, $incentives );
+		$this->assertEquals( 'incentive1', $incentives[0]['id'] );
+		$this->assertEquals( 'incentive2', $incentives[1]['id'] );
+	}
+
+	public function test_get_incentives_not_visible() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive1',
+					'type' => 'type1',
+				),
+				array(
+					'id'   => 'incentive2',
+					'type' => 'type2',
+				),
+			) );
+
+		// None are visible.
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'is_visible' )
+			->willReturn( false );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertCount( 0, $incentives );
+	}
+
+	public function test_get_incentives_with_type() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO', 'type2' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive2',
+					'type' => 'type2',
+				),
+				array(
+					'id'   => 'incentive3',
+					'type' => 'type2',
+				),
+			) );
+
+		// All are visible.
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'is_visible' )
+			->willReturn( true );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO', 'type2' );
+
+		// Assert.
+		$this->assertCount( 2, $incentives );
+		$this->assertEquals( 'incentive2', $incentives[0]['id'] );
+		$this->assertEquals( 'incentive3', $incentives[1]['id'] );
+	}
+
+	public function test_get_incentives_with_type_no_incentives() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO', 'bogus_type' )
+			->willReturn( array() );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO', 'bogus_type' );
+
+		// Assert.
+		$this->assertSame( array(), $incentives );
+	}
+
+	public function test_get_incentives_skips_visibility_checks() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->atLeastOnce() )
+			->method( 'get_all' )
+			->with( 'RO', 'type2' )
+			->willReturn( array(
+				array(
+					'id'   => 'incentive2',
+					'type' => 'type2',
+				),
+				array(
+					'id'   => 'incentive3',
+					'type' => 'type2',
+				),
+			) );
+
+		// Assert.
+		$this->mock_incentive_provider
+			->expects( $this->never() )
+			->method( 'is_visible' );
+
+		// Act.
+		$incentives = $this->sut->get_incentives( 'suggestion1', 'RO', 'type2', true );
+
+		// Assert.
+		$this->assertCount( 2, $incentives );
+		$this->assertEquals( 'incentive2', $incentives[0]['id'] );
+		$this->assertEquals( 'incentive3', $incentives[1]['id'] );
+	}
+
+	public function test_is_incentive_visible_no_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( null );
+
+		// Act.
+		$incentive_visible = $this->sut->is_incentive_visible( 'incentive1', 'suggestion1', 'RO' );
+
+		// Assert.
+		$this->assertFalse( $incentive_visible );
+	}
+
+	public function test_is_incentive_visible() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->once() )
+			->method( 'is_visible' )
+			->with( 'incentive1', 'RO', 'type1', true )
+			->willReturn( true );
+
+		// Act.
+		$this->assertTrue( $this->sut->is_incentive_visible( 'incentive1', 'suggestion1', 'RO', 'type1', true ) );
+	}
+
+	public function test_is_incentive_dismissed_no_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( null );
+
+		// Act.
+		$incentive_dismissed = $this->sut->is_incentive_dismissed( 'incentive1', 'suggestion1' );
+
+		// Assert.
+		$this->assertFalse( $incentive_dismissed );
+	}
+
+	public function test_is_incentive_dismissed() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->once() )
+			->method( 'is_dismissed' )
+			->with( 'incentive1' )
+			->willReturn( true );
+
+		// Act.
+		$incentive_dismissed = $this->sut->is_incentive_dismissed( 'incentive1', 'suggestion1' );
+
+		// Assert.
+		$this->assertTrue( $incentive_dismissed );
+	}
+
+	public function test_is_incentive_dismissed_no() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( $this->mock_incentive_provider );
+
+		$this->mock_incentive_provider
+			->expects( $this->once() )
+			->method( 'is_dismissed' )
+			->with( 'incentive1' )
+			->willReturn( false );
+
+		// Act.
+		$incentive_dismissed = $this->sut->is_incentive_dismissed( 'incentive1', 'suggestion1' );
+
+		// Assert.
+		$this->assertFalse( $incentive_dismissed );
+	}
+
+	public function test_has_incentive_provider_no_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( null );
+
+		// Act.
+		$incentive_provider = $this->sut->has_incentive_provider( 'suggestion1' );
+
+		// Assert.
+		$this->assertFalse( $incentive_provider );
+	}
+
+	public function test_has_incentive_provider() {
+		// Arrange.
+		$this->sut
+			->expects( $this->once() )
+			->method( 'get_incentive_instance' )
+			->with( 'suggestion1' )
+			->willReturn( new FakeIncentive( 'suggestion1' ) );
+
+		// Act.
+		$incentive_provider = $this->sut->has_incentive_provider( 'suggestion1' );
+
+		// Assert.
+		$this->assertTrue( $incentive_provider );
+	}
+}
