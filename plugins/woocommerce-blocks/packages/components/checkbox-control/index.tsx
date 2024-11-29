6fb5236962f1906f7e752e5032bb861cc26de6d3
Thomas Roberts <thomas.roberts@automattic.com>
2024-11-29 15:59:58 +0000

    Fix onChange formatting

--- plugins/woocommerce-blocks/packages/components/checkbox-control/index.tsx
+++ plugins/woocommerce-blocks/packages/components/checkbox-control/index.tsx
@@ -36,61 +36,63 @@ export const CheckboxControl = forwardRef<
 			className,
 			label,
 			id,
-	onChange,
-	children,
-	hasError = false,
-	checked = false,
-	disabled = false,
+			onChange,
+			children,
+			hasError = false,
+			checked = false,
+			disabled = false,
 			feedback,
-	errorId,
-	errorMessage,
-	...rest
+			errorId,
+			errorMessage,
+			...rest
 		}: CheckboxControlProps & Record< string, unknown >,
 		forwardedRef
 	): JSX.Element => {
-	const instanceId = useInstanceId( CheckboxControl );
-	const checkboxId = id || `checkbox-control-${ instanceId }`;
+		const instanceId = useInstanceId( CheckboxControl );
+		const checkboxId = id || `checkbox-control-${ instanceId }`;
 
-	return (
-		<div
-			className={ clsx(
-				'wc-block-components-checkbox',
-				{
-					'has-error': hasError,
-				},
-				className
-			) }
-		>
-			<label htmlFor={ checkboxId }>
-				<input
-						ref={ forwardedRef }
-					id={ checkboxId }
-					className="wc-block-components-checkbox__input"
-					type="checkbox"
-					onChange={ ( event ) => onChange( event.target.checked ) }
-					aria-invalid={ hasError === true }
-					checked={ checked }
-					disabled={ !! disabled }
-					{ ...rest }
-				/>
-				<svg
-					className="wc-block-components-checkbox__mark"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 20"
-				>
-					<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-				</svg>
-				{ label && (
-					<span className="wc-block-components-checkbox__label">
-						{ label }
-					</span>
+		return (
+			<div
+				className={ clsx(
+					'wc-block-components-checkbox',
+					{
+						'has-error': hasError,
+					},
+					className
 				) }
-				{ children }
-			</label>
+			>
+				<label htmlFor={ checkboxId }>
+					<input
+						ref={ forwardedRef }
+						id={ checkboxId }
+						className="wc-block-components-checkbox__input"
+						type="checkbox"
+						onChange={ ( event ) =>
+							onChange( event.target.checked )
+						}
+						aria-invalid={ hasError === true }
+						checked={ checked }
+						disabled={ !! disabled }
+						{ ...rest }
+					/>
+					<svg
+						className="wc-block-components-checkbox__mark"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 20"
+					>
+						<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+					</svg>
+					{ label && (
+						<span className="wc-block-components-checkbox__label">
+							{ label }
+						</span>
+					) }
+					{ children }
+				</label>
 				{ feedback }
-		</div>
-	);
+			</div>
+		);
 	}
 );
 
