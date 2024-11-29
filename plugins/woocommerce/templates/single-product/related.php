a12cc0e92b70e62eeb6e0731473c4b513fb711a3
Vladimir Reznichenko <kalessil@gmail.com>
2024-11-29 09:10:24 +0100

    Monorepo: cleanup.

--- plugins/woocommerce/templates/single-product/related.php
+++ plugins/woocommerce/templates/single-product/related.php
@@ -30,7 +30,6 @@ if ( $related_products ) : ?>
 			?>
 			<h2><?php echo esc_html( $heading ); ?></h2>
 		<?php endif; ?>
-
 		<?php woocommerce_product_loop_start(); ?>
 
 			<?php foreach ( $related_products as $related_product ) : ?>
