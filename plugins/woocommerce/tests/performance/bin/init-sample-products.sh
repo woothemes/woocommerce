06576e9851ed86771dfc044066242519d7ef941a
Vladimir Reznichenko <kalessil@gmail.com>
2024-12-03 15:22:58 +0100

    PoC: profile 1K products

--- plugins/woocommerce/tests/performance/bin/init-sample-products.sh
+++ plugins/woocommerce/tests/performance/bin/init-sample-products.sh
@@ -30,8 +30,8 @@ wp-env run tests-cli wp wc --user=admin tool run install_pages
 # Importing WooCommerce sample products"
 # wp-env run tests-cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
 
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "CREATE TABLE wp_posts_variations LIKE wp_posts;"
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "CREATE TABLE wp_postmeta_variations LIKE wp_postmeta;"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "CREATE TABLE wp_posts_variations LIKE wp_posts;"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "CREATE TABLE wp_postmeta_variations LIKE wp_postmeta;"
 
 # Test on 1K, 10K, 25K, 50k
 # wp-env run tests-cli wp wc generate products 25000 --type=simple &
@@ -46,8 +46,8 @@ echo "Success! Your E2E Test Environment is now ready."
 
 
 # PoC related
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp wc patch
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "REPLACE INTO wp_posts_variations SELECT * FROM wp_posts WHERE post_type='product_variation';"
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "DELETE FROM wp_posts WHERE post_type='product_variation';"
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "REPLACE INTO wp_postmeta_variations SELECT * FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
-pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "DELETE FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp wc patch
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "REPLACE INTO wp_posts_variations SELECT * FROM wp_posts WHERE post_type='product_variation';"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "DELETE FROM wp_posts WHERE post_type='product_variation';"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "REPLACE INTO wp_postmeta_variations SELECT * FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "DELETE FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
