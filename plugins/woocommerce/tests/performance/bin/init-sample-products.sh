08da717a7ddab7ee626da7e2a334057b37eccd2d
Vladimir Reznichenko <kalessil@gmail.com>
2024-12-03 15:07:05 +0100

    PoC: profile 1K products

--- plugins/woocommerce/tests/performance/bin/init-sample-products.sh
+++ plugins/woocommerce/tests/performance/bin/init-sample-products.sh
@@ -7,9 +7,9 @@ wp-env run tests-cli wp plugin activate woocommerce
 wp-env run tests-cli wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=subscriber --path=/var/www/html
 
 # Installing and activating the WordPress Importer plugin to import sample products"
-wp-env run tests-cli wp plugin install wordpress-importer --activate
+# wp-env run tests-cli wp plugin install wordpress-importer --activate
 
-#wp-env run tests-cli wp plugin install https://github.com/woocommerce/wc-smooth-generator/releases/download/1.2.0/wc-smooth-generator.zip --activate
+wp-env run tests-cli wp plugin install https://github.com/woocommerce/wc-smooth-generator/releases/download/1.2.0/wc-smooth-generator.zip --activate
 
 # Adding basic WooCommerce settings"
 wp-env run tests-cli wp option set woocommerce_store_address 'Example Address Line 1'
@@ -28,14 +28,25 @@ wp-env run tests-cli wp option set woocommerce_coming_soon 'no'
 wp-env run tests-cli wp wc --user=admin tool run install_pages
 
 # Importing WooCommerce sample products"
-wp-env run tests-cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
+# wp-env run tests-cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
 
-#wp-env run tests-cli wp wc generate products 25000 --type=simple &
-#wp-env run tests-cli wp wc generate products 25000 --type=simple &
-#wp-env run tests-cli wp wc generate products 25000 --type=simple &
-#wp-env run tests-cli wp wc generate products 25000 --type=simple
+# Test on 1K, 10K, 25K, 50k
+# wp-env run tests-cli wp wc generate products 25000 --type=simple &
+# wp-env run tests-cli wp wc generate products 25000 --type=simple &
+# wp-env run tests-cli wp wc generate products 25000 --type=simple &
+wp-env run tests-cli wp wc generate products 1000
 
 # install Storefront
 wp-env run tests-cli wp theme install storefront --activate
 
 echo "Success! Your E2E Test Environment is now ready."
+
+
+# PoC related
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp wc patch
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "CREATE TABLE wp_posts_variations LIKE wp_posts;"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "REPLACE INTO wp_posts_variations SELECT * FROM wp_posts WHERE post_type='product_variation';"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "DELETE FROM wp_posts WHERE post_type='product_variation';"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "CREATE TABLE wp_postmeta_variations LIKE wp_postmeta;"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "REPLACE INTO wp_postmeta_variations SELECT * FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
+pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "DELETE FROM wp_postmeta WHERE post_id IN ( SELECT ID FROM wp_posts_variations );"
