44af7182d3f05b5e0612e74bda4b8feccd84d546
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2024-12-04 15:56:09 +0900

    Cache valid paths check to avoid duplicate queries

--- plugins/woocommerce/src/Internal/ProductDownloads/ApprovedDirectories/Register.php
+++ plugins/woocommerce/src/Internal/ProductDownloads/ApprovedDirectories/Register.php
@@ -31,6 +31,9 @@ class Register {
 	 */
 	private $mode_option = 'wc_downloads_approved_directories_mode';
 
+	/** @var array<string, bool> cached valid paths */
+	private array $valid_paths = [];
+
 	/**
 	 * Sets up the approved directories sub-system.
 	 *
@@ -240,6 +243,10 @@ class Register {
 	public function is_valid_path( string $download_url ): bool {
 		global $wpdb;
 
+		if ( $download_url && array_key_exists( $download_url, $this->valid_paths ) ) {
+			return $this->valid_paths[ $download_url ];
+		}
+
 		$parent_directories = array();
 
 		foreach ( ( new URL( $this->normalize_url( $download_url ) ) )->get_all_parent_urls() as $parent ) {
@@ -247,6 +254,9 @@ class Register {
 		}
 
 		if ( empty( $parent_directories ) ) {
+
+			$this->valid_paths[ $download_url ] = false;
+
 			return false;
 		}
 
@@ -267,7 +277,9 @@ class Register {
 		);
 		// phpcs:enable
 
-		return $matches > 0;
+		$this->valid_paths[ $download_url ] = $matches > 0;
+
+		return $this->valid_paths[ $download_url ];
 	}
 
 	/**
