1f949c2f24bc063771eb97dfccad9f60dc4dc6cc
Vlad Olaru <vlad@pixelgrade.com>
2024-12-05 09:15:51 +0200

    docs: Reduce comment verbosity

--- plugins/woocommerce/tests/php/src/Internal/Admin/Settings/Mocks/FakePaymentGateway.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Settings/Mocks/FakePaymentGateway.php
@@ -59,17 +59,9 @@ class FakePaymentGateway extends \WC_Payment_Gateway {
 	/**
 	 * Get the recommended payment methods list.
 	 *
-	 * @param string $country_code Optional. The business location country code. Provide a 2-letter ISO country code.
-	 *                             If not provided, the account country will be used if the account is connected.
-	 *                             Otherwise, the store's base country will be used.
+	 * @param string $country_code Optional. The business location country code.
 	 *
 	 * @return array List of recommended payment methods for the given country.
-	 *               Empty array if there are no recommendations available.
-	 *               Each item in the array should be an associative array with at least the following entries:
-	 *               - @string id: The payment method ID.
-	 *               - @string title: The payment method title/name.
-	 *               - @bool enabled: Whether the payment method is enabled.
-	 *               - @int order/priority: The order/priority of the payment method.
 	 */
 	public function get_recommended_payment_methods( string $country_code = '' ): array {
 		return $this->recommended_payment_methods;
