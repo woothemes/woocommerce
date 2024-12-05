a5a099f8af366660f2ebbd0b4edcaa14226eab5f
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-12-05 11:58:25 -0300

    Fix typo in the word sort

--- plugins/woocommerce/includes/wc-template-functions.php
+++ plugins/woocommerce/includes/wc-template-functions.php
@@ -1591,11 +1591,11 @@ if ( ! function_exists( 'woocommerce_catalog_ordering' ) ) {
 				'woocommerce_catalog_orderby',
 				array(
 					'menu_order' => __( 'Default sorting', 'woocommerce' ),
-					'popularity' => __( 'Sor by Popularity', 'woocommerce' ),
-					'rating'     => __( 'Sor by Average rating', 'woocommerce' ),
-					'date'       => __( 'Sor by Latest', 'woocommerce' ),
-					'price'      => __( 'Sor by Price: low to high', 'woocommerce' ),
-					'price-desc' => __( 'Sor by Price: high to low', 'woocommerce' ),
+					'popularity' => __( 'Sort by Popularity', 'woocommerce' ),
+					'rating'     => __( 'Sort by Average rating', 'woocommerce' ),
+					'date'       => __( 'Sort by Latest', 'woocommerce' ),
+					'price'      => __( 'Sort by Price: low to high', 'woocommerce' ),
+					'price-desc' => __( 'Sort by Price: high to low', 'woocommerce' ),
 				)
 			);
 		}
