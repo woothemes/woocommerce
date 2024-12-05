58647c3e70c7418670a1e85cdefe1a31f5473d76
Vladimir Reznichenko <kalessil@gmail.com>
2024-12-05 08:19:56 +0100

    PoC: profile 50K products

--- plugins/woocommerce/tests/performance/bin/init-sample-products.sh
+++ plugins/woocommerce/tests/performance/bin/init-sample-products.sh
@@ -34,11 +34,11 @@ pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "
 pnpm --filter=@woocommerce/plugin-woocommerce wp-env run tests-cli wp db query "CREATE TABLE wp_postmeta_variations LIKE wp_postmeta;"
 
 # Test on 1K, 10K, 25K, 50k
-wp-env run tests-cli wp wc generate products 5000 &
-wp-env run tests-cli wp wc generate products 5000 &
-wp-env run tests-cli wp wc generate products 5000 &
-wp-env run tests-cli wp wc generate products 5000 &
-wp-env run tests-cli wp wc generate products 5000
+wp-env run tests-cli wp wc generate products 10000 &
+wp-env run tests-cli wp wc generate products 10000 &
+wp-env run tests-cli wp wc generate products 10000 &
+wp-env run tests-cli wp wc generate products 10000 &
+wp-env run tests-cli wp wc generate products 10000
 
 # install Storefront
 wp-env run tests-cli wp theme install storefront --activate
