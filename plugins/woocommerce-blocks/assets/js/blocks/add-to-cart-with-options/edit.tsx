b5a11cb20192dd22f4454ac32a30d6e5882ef8dd
Damián Suárez <rdsuarez@gmail.com>
2024-12-05 07:24:11 +0000

    update wrapper button template

--- plugins/woocommerce-blocks/assets/js/blocks/add-to-cart-with-options/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/add-to-cart-with-options/edit.tsx
@@ -39,22 +39,29 @@ const INNER_BLOCKS_TEMPLATE: InnerBlockTemplate[] = [
 		},
 	],
 	[
-		'core/button',
-		{
-			className: 'wc-block-product-add-to-cart-with-options-button',
-			text: __( 'Add to Cart', 'woocommerce' ),
-			withRole: 'add-to-cart-with-options-button',
-			metadata: {
-				bindings: {
-					url: {
-						source: 'core/post-meta',
-						args: {
-							key: 'add_to_cart_url',
+		'core/buttons',
+		{},
+		[
+			[
+				'core/button',
+				{
+					className:
+						'wc-block-product-add-to-cart-with-options-button',
+					text: __( 'Add to Cart', 'woocommerce' ),
+					withRole: 'add-to-cart-with-options-button',
+					metadata: {
+						bindings: {
+							url: {
+								source: 'core/post-meta',
+								args: {
+									key: 'add_to_cart_url',
+								},
+							},
 						},
 					},
 				},
-			},
-		},
+			],
+		],
 	],
 ];
 
