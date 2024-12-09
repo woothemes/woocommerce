8e5fb282fd0b9af7750f17bc4a3132d4aa03ca21
Vladimir Reznichenko <kalessil@gmail.com>
2024-12-09 13:49:01 +0100

    PoC: profile 500 products

--- plugins/woocommerce/tests/performance/bin/init-sample-products.sh
+++ plugins/woocommerce/tests/performance/bin/init-sample-products.sh
@@ -29,7 +29,7 @@ wp-env run tests-cli wp wc --user=admin tool run install_pages
 
 # Importing WooCommerce sample products"
 # wp-env run tests-cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
-wp-env run tests-cli wp wc generate products 100
+wp-env run tests-cli wp wc generate products 500
 
 # install Storefront
 wp-env run tests-cli wp theme install storefront --activate
