eddb3b6586099b909166b2c1421f0b475b5defdc
Fernando Marichal <fernando.marichal@automattic.com>
2024-12-11 12:02:06 -0300

    fix block.tsx

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/stock-indicator/block.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/stock-indicator/block.tsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
 import clsx from 'clsx';
 import {
 	useInnerBlockLayoutContext,
@@ -10,89 +9,12 @@ import {
 import { useStyleProps } from '@woocommerce/base-hooks';
 import { withProductDataContext } from '@woocommerce/shared-hocs';
 import type { HTMLAttributes } from 'react';
-import { ProductResponseItem } from '@woocommerce/types';
-import { getSetting } from '@woocommerce/settings';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 import type { BlockAttributes } from './types';
-
-/**
- * Get stock text based on stock. For example:
- * - In stock
- * - Out of stock
- * - Available on backorder
- * - 2 left in stock
- *
- * @param stockInfo                Object containing stock information.
- * @param stockInfo.isInStock      Whether product is in stock.
- * @param stockInfo.isLowStock     Whether product is low in stock.
- * @param stockInfo.lowStockAmount Number of items left in stock.
- * @param stockInfo.isOnBackorder  Whether product is on backorder.
- * @return string Stock text.
- */
-const getTextBasedOnStock = ( {
-	isInStock = false,
-	isLowStock = false,
-	lowStockAmount = null,
-	isOnBackorder = false,
-}: {
-	isInStock?: boolean;
-	isLowStock?: boolean;
-	lowStockAmount?: number | null;
-	isOnBackorder?: boolean;
-} ): string => {
-	if ( isLowStock && lowStockAmount !== null ) {
-		return sprintf(
-			/* translators: %d stock amount (number of items in stock for product) */
-			__( '%d left in stock', 'woocommerce' ),
-			lowStockAmount
-		);
-	} else if ( isOnBackorder ) {
-		return __( 'Available on backorder', 'woocommerce' );
-	} else if ( isInStock ) {
-		return __( 'In stock', 'woocommerce' );
-	}
-	return __( 'Out of stock', 'woocommerce' );
-};
-
-/**
- * Determines whether the stock indicator should be visible.
- * Stock indicator is not visible when:
- * - Product type is not allowed (variable, grouped, external)
- * - Product is in stock, stock is not managed, and not on backorder
- * - Product is a variation that is on backorder but backorder notification is disabled and stock is managed
- *
- * @param {ProductResponseItem} product Product object
- * @return {boolean} True if stock indicator should be visible
- */
-const isStockVisible = ( product: ProductResponseItem ): boolean => {
-	const productTypesWithoutStockIndicator = getSetting< string[] >(
-		'productTypesWithoutStockIndicator',
-		[ 'external', 'grouped', 'variable' ]
-	);
-
-	const isProductTypeAllowed = ! productTypesWithoutStockIndicator.includes(
-		product.type
-	);
-
-	const isInStockHidden =
-		product.is_in_stock &&
-		product.manage_stock === false &&
-		! product.is_on_backorder;
-
-	const isStockHiddenForVariation =
-		! product.backorder_notification_enabled &&
-		product.is_on_backorder &&
-		product.manage_stock !== false &&
-		product.type === 'variation';
-
-	return (
-		isProductTypeAllowed && ! isInStockHidden && ! isStockHiddenForVariation
-	);
-};
 
 type Props = BlockAttributes & HTMLAttributes< HTMLDivElement >;
 
@@ -102,7 +24,7 @@ export const Block = ( props: Props ): JSX.Element | null => {
 	const { parentClassName } = useInnerBlockLayoutContext();
 	const { product } = useProductDataContext();
 
-	if ( ! product.id || ! isStockVisible( product ) ) {
+	if ( ! product.id || product.stock_indicator_text === '' ) {
 		return null;
 	}
 
@@ -134,12 +56,7 @@ export const Block = ( props: Props ): JSX.Element | null => {
 				style: styleProps.style,
 			} ) }
 		>
-			{ getTextBasedOnStock( {
-				isInStock: inStock,
-				isLowStock: !! lowStock,
-				lowStockAmount: lowStock,
-				isOnBackorder: isBackordered,
-			} ) }
+			{ product.stock_indicator_text }
 		</div>
 	);
 };
