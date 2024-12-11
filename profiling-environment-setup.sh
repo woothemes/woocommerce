02a07bd81e05565502005f62048ef8b6f6f64475
Vladimir Reznichenko <kalessil@gmail.com>
2024-12-11 11:15:26 +0100

    Core: WIP, SQL tuning.

--- profiling-environment-setup.sh
+++ profiling-environment-setup.sh
@@ -12,3 +12,8 @@ pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp plugin install q
 # pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp wc generate products 100
 # pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp wc generate orders 100
 
+# Redefine some PKs for better SQL performance.
+# pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "ALTER TABLE wp_postmeta ADD UNIQUE KEY meta_id (meta_id), DROP PRIMARY KEY, ADD PRIMARY KEY (post_id, meta_key, meta_id), DROP KEY post_id;"
+# pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "ALTER TABLE wp_usermeta ADD UNIQUE KEY umeta_id (umeta_id), DROP PRIMARY KEY, ADD PRIMARY KEY (user_id, meta_key, umeta_id), DROP KEY user_id;"
+# pnpm --filter=@woocommerce/plugin-woocommerce wp-env run cli wp db query "ALTER TABLE wp_options ADD UNIQUE KEY option_id (option_id), DROP PRIMARY KEY, ADD PRIMARY KEY (option_name), DROP KEY option_name;"
+
