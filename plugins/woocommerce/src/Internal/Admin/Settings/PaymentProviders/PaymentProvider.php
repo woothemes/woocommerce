c322f0c53896d7b3cbd51f7a6f5f9073fc2a4fdd
Vlad Olaru <vlad@pixelgrade.com>
2024-12-11 21:52:34 +0200

    Add stub provider classes

---
+++ plugins/woocommerce/src/Internal/Admin/Settings/PaymentProviders/PaymentProvider.php
@@ -0,0 +1,28 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Internal\Admin\Settings\PaymentProviders;
+
+/**
+ * Abstract class for payment provider classes.
+ */
+abstract class PaymentProvider {
+
+	/**
+	 * The single instance of the payment provider class.
+	 */
+	protected static PaymentProvider $instance;
+
+	/**
+	 * Get the instance of the class.
+	 *
+	 * @return static
+	 */
+	public static function get_instance(): self {
+		if ( ! isset( static::$instance ) ) {
+			static::$instance = new static();
+		}
+
+		return static::$instance;
+	}
+}
