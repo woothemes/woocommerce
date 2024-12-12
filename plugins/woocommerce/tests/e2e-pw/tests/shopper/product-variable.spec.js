b26d30ac24ce08968d7e717840356be1dd5be0a7
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-12-12 17:35:32 +0800

    Fix e2e tests

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/product-variable.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/product-variable.spec.js
@@ -412,12 +412,12 @@ test.describe(
 
 			await expect(
 				page.locator( '.woocommerce-product-attributes-item--weight' )
-			).toContainText( '400 kg' );
+			).toContainText( '400 lbs' );
 			await expect(
 				page.locator(
 					'.woocommerce-product-attributes-item--dimensions'
 				)
-			).toContainText( '20 × 40 × 30 cm' );
+			).toContainText( '20 × 40 × 30 in' );
 		} );
 
 		test( 'Shopper can change variable product attributes to variation with a different price', async ( {
