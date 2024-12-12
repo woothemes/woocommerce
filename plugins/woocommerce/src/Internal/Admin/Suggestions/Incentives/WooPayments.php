8f31faff7c9c32b9863eed8ad3ea7faaf08459e1
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2024-12-12 10:46:04 +0200

    Revert patch hard-coded values

--- plugins/woocommerce/src/Internal/Admin/Suggestions/Incentives/WooPayments.php
+++ plugins/woocommerce/src/Internal/Admin/Suggestions/Incentives/WooPayments.php
@@ -133,11 +133,11 @@ class WooPayments extends Incentive {
 			// Store locale, e.g. `en_US`.
 			'locale'       => get_locale(),
 			// WooCommerce store active for duration in seconds.
-			'active_for'   => 8000000,
-			'has_orders'   => true,
+			'active_for'   => WCAdminHelper::get_wcadmin_active_for_in_seconds(),
+			'has_orders'   => $this->has_orders(),
 			// Whether the store has at least one payment gateway enabled.
-			'has_payments' => true,
-			'has_wcpay'    => false,
+			'has_payments' => ! empty( WC()->payment_gateways()->get_available_payment_gateways() ),
+			'has_wcpay'    => $this->has_wcpay(),
 		);
 
 		// Fingerprint the store context through a hash of certain entries.
