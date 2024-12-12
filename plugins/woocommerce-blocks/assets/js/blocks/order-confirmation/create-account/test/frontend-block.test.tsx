f6ad585a1074e5f2ae7e006b5db7ae7a0aeb6f53
Mike Jolley <mike.jolley@me.com>
2024-12-12 12:45:15 +0000

    update test

--- plugins/woocommerce-blocks/assets/js/blocks/order-confirmation/create-account/test/frontend-block.test.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/order-confirmation/create-account/test/frontend-block.test.tsx
@@ -149,7 +149,7 @@ describe( 'CreateAccountFrontendBlock - Automatic password generation on', () =>
 			expect(
 				await queryByText(
 					textContentMatcher(
-						"We'll email test@test.com a link to set up your account password."
+						'Check your email at test@test.com for the link to set up an account password.'
 					)
 				)
 			).toBeInTheDocument();
