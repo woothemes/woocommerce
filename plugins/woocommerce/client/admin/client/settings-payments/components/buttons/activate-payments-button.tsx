178428a4b45df9351670a5b67fa42c1c6b965e6a
Daniel Mallory <daniel.mallory@automattic.com>
2024-12-14 13:37:20 +0000

    Refactors to payment method buttons and behaviour.

--- plugins/woocommerce/client/admin/client/settings-payments/components/buttons/activate-payments-button.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/buttons/activate-payments-button.tsx
@@ -4,7 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { useState } from '@wordpress/element';
-import { PaymentIncentive, RecommendedPaymentMethod } from '@woocommerce/data';
+import { PaymentIncentive } from '@woocommerce/data';
 
 /**
  * Internal dependencies
