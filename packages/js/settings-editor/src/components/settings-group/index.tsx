526c515c16e3a032e9a57d7a73dfc91b03c80637
paul sealock <psealock@gmail.com>
2024-12-17 09:59:53 +1300

    format links

--- packages/js/settings-editor/src/components/settings-group/index.tsx
+++ packages/js/settings-editor/src/components/settings-group/index.tsx
@@ -4,13 +4,22 @@
 import { createElement } from '@wordpress/element';
 // @ts-expect-error missing types.
 import { __experimentalHeading as Heading } from '@wordpress/components';
+import { sanitize } from 'dompurify';
+
+const ALLOWED_TAGS = [ 'a', 'b', 'em', 'i', 'strong', 'p', 'br' ];
+const ALLOWED_ATTR = [ 'target', 'href', 'rel', 'name', 'download' ];
 
 export const SettingsGroup = ( { group }: { group: SettingsGroup } ) => {
+	const sanitizeHTML = ( html: string ) => {
+		return {
+			__html: sanitize( html, { ALLOWED_TAGS, ALLOWED_ATTR } ),
+		};
+	};
 	return (
 		<div className="woocommerce-settings-group">
 			<div className="woocommerce-settings-group-title">
 				<Heading level={ 4 }>{ group.title }</Heading>
-				<p>{ group.desc }</p>
+				<p dangerouslySetInnerHTML={ sanitizeHTML( group.desc ) } />
 			</div>
 			<div className="woocommerce-settings-group-content">
 				{ group.settings.map( ( setting ) => (
