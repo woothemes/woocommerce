8f5465d087e7dc07c19bf529255e09c0df6a0783
paul sealock <psealock@gmail.com>
2024-12-17 10:57:14 +1300

    add a save button

--- packages/js/settings-editor/src/legacy/content.tsx
+++ packages/js/settings-editor/src/legacy/content.tsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import { createElement } from '@wordpress/element';
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -22,14 +24,23 @@ export const LegacyContent = ( {
 	}
 
 	return (
-		<div className="woocommerce-settings-content">
-			{ section.settings.map( ( group ) => {
-				if ( group.type === 'group' ) {
-					return <SettingsGroup key={ group.id } group={ group } />;
-				}
-				// Handle settings not in a group here.
-				return null;
-			} ) }
-		</div>
+		<form>
+			<div className="woocommerce-settings-content">
+				{ section.settings.map( ( group ) => {
+					if ( group.type === 'group' ) {
+						return (
+							<SettingsGroup key={ group.id } group={ group } />
+						);
+					}
+					// Handle settings not in a group here.
+					return null;
+				} ) }
+			</div>
+			<div className="woocommerce-settings-content-footer">
+				<Button variant="primary">
+					{ __( 'Save', 'woocommerce' ) }
+				</Button>
+			</div>
+		</form>
 	);
 };
