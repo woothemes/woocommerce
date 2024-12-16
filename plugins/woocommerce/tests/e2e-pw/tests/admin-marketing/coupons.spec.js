e0e4db82b7df5559bd4deeb8546999d939b62611
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2024-12-16 16:49:59 +0800

    Add tag and TODO comment.

--- plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/coupons.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/coupons.spec.js
@@ -1,24 +1,28 @@
 const { test, expect } = require( '@playwright/test' );
+const { tags } = require( '../../fixtures/fixtures' );
 
 test.describe( 'Coupons page', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
-	test( 'A user can view the coupons overview without it crashing', async ( {
-		page,
-	} ) => {
-		await page.goto(
-			'wp-admin/edit.php?post_type=shop_coupon&legacy_coupon_menu=1'
-		);
+	// TODO (E2E Audit): This test can be combined with the other tests in the overview.spec.js to be more efficient. Test title should clarify that the page being tested here is the
+	test(
+		'A user can view the coupons overview without it crashing',
+		{ tag: [ tags.NOT_E2E ] },
+		async ( { page } ) => {
+			await page.goto(
+				'wp-admin/edit.php?post_type=shop_coupon&legacy_coupon_menu=1'
+			);
 
-		await expect( page.locator( 'h1.wp-heading-inline' ) ).toHaveText(
-			'Coupons'
-		);
+			await expect( page.locator( 'h1.wp-heading-inline' ) ).toHaveText(
+				'Coupons'
+			);
 
-		// Use regex to allow for "Add coupon" or "Add new coupon"
-		// making it compatible with WP <=6.6 & 6.7+
-		const addCouponRegex = /Add (coupon|new coupon)/i;
-		await expect( page.locator( 'a.page-title-action' ) ).toHaveText(
-			addCouponRegex
-		);
-	} );
+			// Use regex to allow for "Add coupon" or "Add new coupon"
+			// making it compatible with WP <=6.6 & 6.7+
+			const addCouponRegex = /Add (coupon|new coupon)/i;
+			await expect( page.locator( 'a.page-title-action' ) ).toHaveText(
+				addCouponRegex
+			);
+		}
+	);
 } );
