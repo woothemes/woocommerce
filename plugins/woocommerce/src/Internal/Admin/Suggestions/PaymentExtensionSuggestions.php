ad9515c7306cb90b6ed4ff3dce88653083d1cdb8
Vlad Olaru <vlad@pixelgrade.com>
2024-12-19 20:01:53 +0200

    Lint fix

--- plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentExtensionSuggestions.php
+++ plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentExtensionSuggestions.php
@@ -122,7 +122,7 @@ class PaymentExtensionSuggestions {
 			self::KLARNA,
 		),
 		'US' => array(
-			self::WOOPAYMENTS  => array(
+			self::WOOPAYMENTS => array(
 				'_append' => array(
 					'tags' => array( 'woopay_eligible' ), // Add a special tag that will be used to determine if the merchant is eligible for WooPay.
 				),
