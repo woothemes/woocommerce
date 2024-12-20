ea64506e3bd7ec2c75fa5b6df21de34c774e08bd
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2024-12-20 23:30:24 +0800

    Improve spacing to match design

--- plugins/woocommerce/patterns/page-coming-soon-image-gallery.php
+++ plugins/woocommerce/patterns/page-coming-soon-image-gallery.php
@@ -54,16 +54,12 @@ $featured_image_urls = array(
 			<!-- /wp:group --></div>
 		<!-- /wp:group -->
 
-		<!-- wp:spacer {"height":"var:preset|spacing|50","style":{"layout":{}}} -->
-		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
-		<!-- /wp:spacer -->
-
 		<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
 		<div class="wp-block-group alignwide">
-			<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"85px","bottom":"85px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group alignwide">
-				<!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"48px","lineHeight":"1.3","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"<?php echo esc_html( $cardo_font_family ); ?>"} -->
-					<h1 class="wp-block-heading has-<?php echo esc_html( $cardo_font_family ); ?>-font-family" style="font-size:48px;font-style:normal;font-weight:400;line-height:1.3"><em><?php echo esc_html__( 'Great things are coming soon', 'woocommerce' ); ?></em></h1>
+				<!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"48px","lineHeight":"1.3","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"top":"85px","bottom":"85px"}}},"fontFamily":"heading"} -->
+					<h1 class="wp-block-heading has-heading-font-family" style="padding-top:85px;padding-bottom:85px;font-size:48px;font-style:normal;font-weight:400;line-height:1.3"><em>Great things are coming soon</em></h1>
 				<!-- /wp:heading -->
 			</div>
 			<!-- /wp:group -->
@@ -73,8 +69,8 @@ $featured_image_urls = array(
 	<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"},"tagName":"main"} -->
 	<main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
 
-		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--50)">
 			<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}}} -->
 			<div class="wp-block-columns alignwide" style="margin-top:0;margin-bottom:0">
 				<!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
