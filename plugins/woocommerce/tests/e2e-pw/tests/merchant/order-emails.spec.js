17dedb83f45f61d0ce8cba42bbd3252fe8557bdf
Ivan Stojadinov <ivan.stojadinov@automattic.com>
2024-12-24 09:10:53 +0100

    Remove tags.only

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/order-emails.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/order-emails.spec.js
@@ -6,7 +6,7 @@ const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 
 test.describe(
 	'Merchant > Order Action emails received',
-	{ tag: [ tags.SERVICES, tags.HPOS, tags.ONLY ] },
+	{ tag: [ tags.SERVICES, tags.HPOS ] },
 	() => {
 		test.use( { storageState: process.env.ADMINSTATE } );
 
