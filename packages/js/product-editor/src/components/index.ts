2a2b338cd986c4666d35147707dbcf2ae29124e9
Lourens Schep <lourensschep@gmail.com>
2025-01-03 12:08:15 +0100

    Remove unused experimental and deprecated product editor related components

--- packages/js/product-editor/src/components/index.ts
+++ packages/js/product-editor/src/components/index.ts
@@ -2,9 +2,6 @@ export {
 	ProductSectionLayout as __experimentalProductSectionLayout,
 	ProductFieldSection as __experimentalProductFieldSection,
 } from './product-section-layout';
-export { WooProductFieldItem as __experimentalWooProductFieldItem } from './woo-product-field-item';
-export { WooProductSectionItem as __experimentalWooProductSectionItem } from './woo-product-section-item';
-export { WooProductTabItem as __experimentalWooProductTabItem } from './woo-product-tab-item';
 export { DetailsNameField as __experimentalDetailsNameField } from './details-name-field';
 export { DetailsFeatureField as __experimentalDetailsFeatureField } from './details-feature-field';
 export { DetailsSummaryField as __experimentalDetailsSummaryField } from './details-summary-field';
