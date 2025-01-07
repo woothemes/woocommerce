028543564e64f2eeabb9b120d88cfeba22a04e27
Jorge Torres <jorge.torres@automattic.com>
2025-01-07 07:52:11 -0300

    Correct version in docblock

--- plugins/woocommerce/includes/wc-stock-functions.php
+++ plugins/woocommerce/includes/wc-stock-functions.php
@@ -491,7 +491,7 @@ add_action( 'woocommerce_order_status_on-hold', 'wc_release_stock_for_order', 11
 /**
  * Release coupons used for another order.
  *
- * @since 9.4.0
+ * @since 9.5.2
  * @param \WC_Order|int $order Order ID or instance.
  * @param bool          $save Save the order after releasing coupons.
  */
