857f9515d48283b23314ca7efd584970fd8968c5
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-07 11:12:36 +0000

    Enable LoadingMask for shipping packages when extensionCartUpdate is running

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-rates-control/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-rates-control/index.tsx
@@ -83,7 +83,8 @@ const ShippingRatesControl = ( {
 	// Prepare props to pass to the ExperimentalOrderShippingPackages slot fill.
 	// We need to pluck out receiveCart.
 	// eslint-disable-next-line no-unused-vars
-	const { extensions, receiveCart, ...cart } = useStoreCart();
+	const { isApplyingExtensionCartUpdate, extensions, receiveCart, ...cart } =
+		useStoreCart();
 	const slotFillProps = {
 		className,
 		collapsible,
@@ -110,7 +111,7 @@ const ShippingRatesControl = ( {
 
 	return (
 		<LoadingMask
-			isLoading={ isLoadingRates }
+			isLoading={ isLoadingRates || isApplyingExtensionCartUpdate }
 			screenReaderLabel={ __( 'Loading shipping rates…', 'woocommerce' ) }
 			showSpinner={ true }
 		>
