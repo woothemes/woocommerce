907b878a2792fda714a54c196dbd14fc4dc552f1
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-01-09 14:03:14 +0100

    Sanitize HTML in Summary component

--- plugins/woocommerce-blocks/assets/js/base/components/summary/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/summary/index.tsx
@@ -3,6 +3,7 @@
  */
 import { RawHTML, useMemo } from '@wordpress/element';
 import { WordCountType } from '@woocommerce/block-settings';
+import { sanitizeHTML } from '@woocommerce/utils';
 import type { CSSProperties } from 'react';
 
 /**
@@ -17,6 +18,38 @@ interface SummaryProps {
 	countType?: WordCountType;
 	style?: CSSProperties;
 }
+
+const allowedTags = [
+	'a',
+	'b',
+	'em',
+	'i',
+	'strong',
+	'p',
+	'br',
+	'ul',
+	'ol',
+	'li',
+	'h1',
+	'h2',
+	'h3',
+	'h4',
+	'h5',
+	'h6',
+	'pre',
+	'blockquote',
+	'img',
+];
+
+const allowedAttributes = [
+	'target',
+	'href',
+	'rel',
+	'name',
+	'download',
+	'src',
+];
+
 /**
  * Summary component.
  *
@@ -35,13 +68,17 @@ export const Summary = ( {
 	className = '',
 	style = {},
 }: SummaryProps ): JSX.Element => {
+	console.log( 'source', source );
 	const summaryText = useMemo( () => {
 		return generateSummary( source, maxLength, countType );
 	}, [ source, maxLength, countType ] );
 
 	return (
 		<RawHTML style={ style } className={ className }>
-			{ summaryText }
+			{ sanitizeHTML( summaryText, {
+				tags: allowedTags,
+				attr: allowedAttributes,
+			} ) }
 		</RawHTML>
 	);
 };
