80d7cc058edffe6e0a0da3d64d8ccfccf07ed041
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-09 10:13:36 +0000

    Remove type augmentation from data stores

--- plugins/woocommerce-blocks/assets/js/data/store-notices/index.ts
+++ plugins/woocommerce-blocks/assets/js/data/store-notices/index.ts
@@ -9,7 +9,6 @@ import { createReduxStore, register } from '@wordpress/data';
 import * as actions from './actions';
 import * as selectors from './selectors';
 import reducer from './reducers';
-import { DispatchFromMap, SelectFromMap } from '../mapped-types';
 
 const STORE_KEY = 'wc/store/store-notices';
 const config = {
@@ -21,14 +20,3 @@ const store = createReduxStore( STORE_KEY, config );
 register( store );
 
 export const STORE_NOTICES_STORE_KEY = STORE_KEY;
-
-declare module '@wordpress/data' {
-	function dispatch(
-		key: typeof STORE_KEY
-	): DispatchFromMap< typeof actions >;
-	function select( key: typeof STORE_KEY ): SelectFromMap<
-		typeof selectors
-	> & {
-		hasFinishedResolution: ( selector: string ) => boolean;
-	};
-}
