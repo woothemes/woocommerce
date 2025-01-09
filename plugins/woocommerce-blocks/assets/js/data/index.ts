9357e9ae7e4c58338c788f84908fa655cd685e1d
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-09 14:33:33 +0000

    Export the "plain JS" store descriptions

--- plugins/woocommerce-blocks/assets/js/data/index.ts
+++ plugins/woocommerce-blocks/assets/js/data/index.ts
@@ -8,10 +8,13 @@ import '@wordpress/notices';
  */
 export { CART_STORE_KEY, store as cartStore } from './cart';
 export { CHECKOUT_STORE_KEY, store as checkoutStore } from './checkout';
-export { COLLECTIONS_STORE_KEY } from './collections';
+export {
+	COLLECTIONS_STORE_KEY,
+	store as collectionsStore,
+} from './collections';
 export { PAYMENT_STORE_KEY, store as paymentStore } from './payment';
-export { QUERY_STATE_STORE_KEY } from './query-state';
-export { SCHEMA_STORE_KEY } from './schema';
+export { QUERY_STATE_STORE_KEY, store as queryStateStore } from './query-state';
+export { SCHEMA_STORE_KEY, store as schemaStore } from './schema';
 export {
 	STORE_NOTICES_STORE_KEY,
 	store as storeNoticesStore,
