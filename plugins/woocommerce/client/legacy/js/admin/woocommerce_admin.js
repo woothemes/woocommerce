12f978fb300e50948d5e96c4475a6bb06778abaa
Eliasu Abraman <e.abraman@inpsyde.com>
2025-01-14 12:13:59 +0000

    Ensure GTIN,UPC, EAN, or ISBN field never starts or ends with a dash
    This is a fix to ensure that on product edit page, GTIN,UPC, EAN, or ISBN field does not allow for a value that starts or ends with a dash. It extends the existing functionality from replacing invalid characters with empty strings  to making sure dashes at the begining and end are stripped too.
    
    This is necessary because the existing solution allows values such as ABC-123 or 123-ABC which are not valid inputs for GTIN,UPC, EAN, or ISBN.

--- plugins/woocommerce/client/legacy/js/admin/woocommerce_admin.js
+++ plugins/woocommerce/client/legacy/js/admin/woocommerce_admin.js
@@ -325,13 +325,16 @@
 				'input[type=text][name*=_global_unique_id]',
 				function () {
 					var global_unique_id = $( this ).val();
+
 					$( this ).val(
-						global_unique_id.replace( /[^0-9\-]/g, '' )
+						global_unique_id
+							.replace( /[^0-9\-]/g, '' )
+							.replace( /^-+|-+$/g, '' )
 					);
-					$( document.body ).triggerHandler(
-						'wc_remove_error_tip',
-						[ $( this ), 'i18n_global_unique_id_error' ]
-					);
+					$( document.body ).triggerHandler( 'wc_remove_error_tip', [
+						$( this ),
+						'i18n_global_unique_id_error',
+					] );
 				}
 			)
 
