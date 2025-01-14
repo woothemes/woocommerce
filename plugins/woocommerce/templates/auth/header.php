c0d981ccc5cdbe242dab1f20cbfe391e6d23a47a
And Finally <andfinally@users.noreply.github.com>
2025-01-14 10:38:26 +0000

    Template version bump.

--- plugins/woocommerce/templates/auth/header.php
+++ plugins/woocommerce/templates/auth/header.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Auth
- * @version 9.6.0
+ * @version 9.7.0
  */
 
 use Automattic\WooCommerce\Internal\BrandingController;
