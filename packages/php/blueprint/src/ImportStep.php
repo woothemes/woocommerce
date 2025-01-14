8c67255a5dd0094f528f0cd2b2af25f2ae342ca5
moon <moon.kyong@automattic.com>
2025-01-14 15:54:44 -0800

    Add /wp-json/wc-admin/blueprint/import-step endpoint

--- packages/php/blueprint/src/ImportStep.php
+++ packages/php/blueprint/src/ImportStep.php
@@ -2,27 +2,20 @@
 
 namespace Automattic\WooCommerce\Blueprint;
 
-use Automattic\WooCommerce\Blueprint\Schemas\JsonSchema;
-use Automattic\WooCommerce\Blueprint\Schemas\ZipSchema;
 use Opis\JsonSchema\Errors\ErrorFormatter;
 use Opis\JsonSchema\Validator;
 
 /**
- * Class ImportSchema
+ * Class ImportStep
  *
- * Handles the import schema functionality for WooCommerce.
+ * Import a signle step from a JSON definition.
  *
  * @package Automattic\WooCommerce\Blueprint
  */
-class ImportSchema {
+class ImportStep {
 	use UseWPFunctions;
 
-	/**
-	 * JsonSchema object.
-	 *
-	 * @var JsonSchema The schema instance.
-	 */
-	private JsonSchema $schema;
+	private object $step_definition;
 
 	/**
 	 * Validator object.
@@ -38,154 +31,66 @@ class ImportSchema {
 	 */
 	private BuiltInStepProcessors $builtin_step_processors;
 
-	/**
-	 * ImportSchema constructor.
-	 *
-	 * @param JsonSchema     $schema The schema instance.
-	 * @param Validator|null $validator The validator instance, optional.
-	 */
-	public function __construct( JsonSchema $schema, ?Validator $validator = null ) {
-		$this->schema = $schema;
+	private array $importers;
+	private array $indexed_importers;
+
+
+	public function __construct( $step_definition, ?Validator $validator = null ) {
+		$this->step_definition = $step_definition;
 		if ( null === $validator ) {
 			$validator = new Validator();
 		}
-
-		$this->validator = $validator;
-
-		$this->builtin_step_processors = new BuiltInStepProcessors( $schema );
-	}
-
-	/**
-	 * Get the schema.
-	 *
-	 * @return JsonSchema The schema.
-	 */
-	public function get_schema() {
-		return $this->schema;
-	}
-
-	/**
-	 * Create an ImportSchema instance from a file.
-	 *
-	 * @param string $file The file path.
-	 * @return ImportSchema The created ImportSchema instance.
-	 */
-	public static function create_from_file( $file ) {
-		// @todo check for mime type
-		// @todo check for allowed types -- json or zip
-		$path_info = pathinfo( $file );
-		$is_zip    = 'zip' === $path_info['extension'];
-
-		return $is_zip ? self::create_from_zip( $file ) : self::create_from_json( $file );
-	}
-
-	/**
-	 * Create an ImportSchema instance from a JSON file.
-	 *
-	 * @param string $json_path The JSON file path.
-	 * @return ImportSchema The created ImportSchema instance.
-	 */
-	public static function create_from_json( $json_path ) {
-		return new self( new JsonSchema( $json_path ) );
-	}
-
-	/**
-	 * Create an ImportSchema instance from a ZIP file.
-	 *
-	 * @param string $zip_path The ZIP file path.
-	 *
-	 * @return ImportSchema The created ImportSchema instance.
-	 */
-	public static function create_from_zip( $zip_path ) {
-		return new self( new ZipSchema( $zip_path ) );
+		$this->validator         = $validator;
+		$this->importers         = $this->wp_apply_filters( 'wooblueprint_importers', ( ( new BuiltInStepProcessors() )->get_all() ));
+		$this->indexed_importers = Util::index_array(
+			$this->importers,
+			function ( $key, $importer ) {
+				return $importer->get_step_class()::get_step_name();
+			}
+		);
 	}
 
 	/**
 	 * Import the schema steps.
 	 *
-	 * @return StepProcessorResult[]
+	 * @return StepProcessorResult
 	 */
 	public function import() {
-		$results   = array();
-		$result    = StepProcessorResult::success( 'ImportSchema' );
-		$results[] = $result;
+		$result = StepProcessorResult::success( 'ImportStep' );
 
-		$step_processors = $this->builtin_step_processors->get_all();
+		if ( ! isset( $this->indexed_importers[ $this->step_definition->step ] ) ) {
+			$result->add_error( "Unable to find a impoter for {$this->step_definition->step}" );
+			return $result;
+		}
 
-		/**
-		 * Filters the step processors.
-		 *
-		 * Allows adding/removing custom step processors.
-		 *
-		 * @param StepProcessor[] $step_processors The step processors.
-		 *
-		 * @since 0.0.1
-		 */
-		$step_processors = $this->wp_apply_filters( 'wooblueprint_importers', $step_processors );
-
-		$indexed_step_processors = Util::index_array(
-			$step_processors,
-			function ( $key, $step_processor ) {
-				return $step_processor->get_step_class()::get_step_name();
-			}
-		);
+		$importer = $this->indexed_importers[ $this->step_definition->step ];
 
 		// validate steps before processing.
-		$this->validate_step_schemas( $indexed_step_processors, $result );
+		$this->validate_step_schemas( $importer, $result );
 
 		if ( count( $result->get_messages( 'error' ) ) !== 0 ) {
-			return $results;
+			return $result;
 		}
 
-		foreach ( $this->schema->get_steps() as $step_schema ) {
-			$step_processor = $indexed_step_processors[ $step_schema->step ] ?? null;
-			if ( ! $step_processor instanceof StepProcessor ) {
-				$result->add_error( "Unable to create a step processor for {$step_schema->step}" );
-				continue;
-			}
+		$result->merge_messages( $importer->process( $this->step_definition ) );
 
-			$results[] = $step_processor->process( $step_schema );
-		}
-
-		return $results;
+		return $result;
 	}
 
-	/**
-	 * Validate the step schemas.
-	 *
-	 * @param array               $indexed_step_processors Array of step processors indexed by step name.
-	 * @param StepProcessorResult $result The result object to add messages to.
-	 *
-	 * @return void
-	 */
-	protected function validate_step_schemas( array $indexed_step_processors, StepProcessorResult $result ) {
-		$step_schemas = array_map(
-			function ( $step_processor ) {
-				return $step_processor->get_step_class()::get_schema();
-			},
-			$indexed_step_processors
-		);
+	protected function validate_step_schemas( StepProcessor $importer, StepProcessorResult $result ) {
+		$step_schema    = call_user_func( [ $importer->get_step_class(), 'get_schema' ] );
 
-		foreach ( $this->schema->get_steps() as $step_json ) {
-			$step_schema = $step_schemas[ $step_json->step ] ?? null;
-			if ( ! $step_schema ) {
-				$result->add_info( "No schema found for step $step_json->step" );
-				continue;
+		$validate = $this->validator->validate( $this->step_definition, json_encode( $step_schema ) );
+
+		if ( ! $validate->isValid() ) {
+			$result->add_error( "Schema validation failed for step {$step_json->step}" );
+			$errors           = ( new ErrorFormatter() )->format( $validate->error() );
+			$formatted_errors = array();
+			foreach ( $errors as $value ) {
+				$formatted_errors[] = implode( "\n", $value );
 			}
 
-			// phpcs:ignore
-			$validate = $this->validator->validate( $step_json, json_encode( $step_schema ) );
-
-			if ( ! $validate->isValid() ) {
-				$result->add_error( "Schema validation failed for step {$step_json->step}" );
-				$errors           = ( new ErrorFormatter() )->format( $validate->error() );
-				$formatted_errors = array();
-				foreach ( $errors as $value ) {
-					$formatted_errors[] = implode( "\n", $value );
-				}
-
-				$result->add_error( implode( "\n", $formatted_errors ) );
-			}
+			$result->add_error( implode( "\n", $formatted_errors ) );
 		}
 	}
 }
