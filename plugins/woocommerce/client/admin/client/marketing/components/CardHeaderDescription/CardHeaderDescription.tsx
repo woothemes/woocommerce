05870e51d8812a2660c91bdca60ff47be7945c69
Gan Eng Chin <ecgan@users.noreply.github.com>
2025-01-14 18:25:34 +0800

    Fix TS error in CardHeaderDescription.

--- plugins/woocommerce/client/admin/client/marketing/components/CardHeaderDescription/CardHeaderDescription.tsx
+++ plugins/woocommerce/client/admin/client/marketing/components/CardHeaderDescription/CardHeaderDescription.tsx
@@ -3,7 +3,13 @@
  */
 import './CardHeaderDescription.scss';
 
-export const CardHeaderDescription: React.FC = ( { children } ) => {
+interface CardHeaderDescriptionProps {
+	children: React.ReactNode;
+}
+
+export const CardHeaderDescription: React.FC< CardHeaderDescriptionProps > = ( {
+	children,
+} ) => {
 	return (
 		<div className="woocommerce-marketing-card-header-description">
 			{ children }
