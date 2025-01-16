af575178b1adb71574ac0d2d392cd10fb459682d
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-01-16 15:29:19 +0100

    Remove unnecessary editor class

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-pager/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-pager/edit.tsx
@@ -2,14 +2,10 @@
  * External dependencies
  */
 import { useBlockProps } from '@wordpress/block-editor';
-import clsx from 'clsx';
 
 export const Edit = (): JSX.Element => {
 	const blockProps = useBlockProps( {
-		className: clsx(
-			'wc-block-editor-product-gallery-pager',
-			'wc-block-product-gallery-pager'
-		),
+		className: 'wc-block-product-gallery-pager',
 	} );
 
 	return <div { ...blockProps }>3 / 7</div>;
