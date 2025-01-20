6f7a2b3ca3085651b3e9dc49b1d169542fbcb4f5
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-20 13:08:55 +0000

    Add postinstall script to install wp-prettier and eslint-plugin-prettier

--- packages/js/extend-cart-checkout-block/index.js
+++ packages/js/extend-cart-checkout-block/index.js
@@ -1,6 +1,10 @@
 module.exports = {
 	templatesPath: __dirname,
 	defaultValues: {
+		customScripts: {
+			postinstall:
+				'npm i --D "prettier@npm:wp-prettier@latest" && npm i --D eslint-plugin-prettier@latest',
+		},
 		npmDevDependencies: [
 			'@woocommerce/dependency-extraction-webpack-plugin',
 			'@woocommerce/eslint-plugin',
