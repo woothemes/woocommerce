6c2a976719c220cb4519f173858cd9e3fa787683
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-21 00:19:00 +0000

    Export store config in separate file

--- plugins/woocommerce-blocks/assets/js/data/payment/index.ts
+++ plugins/woocommerce-blocks/assets/js/data/payment/index.ts
@@ -1,28 +1,8 @@
 /**
- * External dependencies
- */
-import { createReduxStore, register } from '@wordpress/data';
-import { controls as dataControls } from '@wordpress/data-controls';
-
-/**
  * Internal dependencies
  */
-import reducer from './reducers';
-import { STORE_KEY } from './constants';
-import * as actions from './actions';
-import { controls as sharedControls } from '../shared-controls';
-import * as selectors from './selectors';
+export { config, store, PAYMENT_STORE_KEY } from './store-config';
 
-export const config = {
-	reducer,
-	selectors,
-	actions,
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	controls: { ...dataControls, ...sharedControls } as any,
-	__experimentalUseThunks: true,
-};
-
-export const store = createReduxStore( STORE_KEY, config );
-register( store );
-
-export const PAYMENT_STORE_KEY = STORE_KEY;
+// This file is different to the other data stores as several other of our data stores subscribe to it, and we need
+// to be able to import the store config without introducing a circular dependency. That is why the store config is
+// in its own file, and we export it here.
