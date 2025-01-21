b0e24ee654aa2e4f68d74877911ec133f925d654
Lourens Schep <lourensschep@gmail.com>
2025-01-21 08:58:57 +0100

    Use wc_initial_state

--- plugins/woocommerce/src/Blocks/BlockTypes/AccordionItem.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AccordionItem.php
@@ -30,7 +30,7 @@ class AccordionItem extends AbstractBlock {
 		
 			// Initialize the state of the item on the server using a closure,
 			// since we need to get derived state based on the current context.
-			wp_interactivity_state(
+			wc_initial_state(
 				'woocommerce/accordion',
 				array(
 					'isOpen' => function () {
