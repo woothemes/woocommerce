bdb7e12ade60f96064e2ef0a60d92529f86d9d44
Tom Cafferkey <tjcafferkey@gmail.com>
2025-01-22 09:35:13 +0000

    Remove unnecessry clx usage

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/save.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/save.tsx
@@ -2,11 +2,10 @@
  * External dependencies
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
-import clsx from 'clsx';
 
 export const Save = () => {
 	const blockProps = useBlockProps.save( {
-		className: clsx( 'wc-block-product-gallery' ),
+		className: 'wc-block-product-gallery',
 	} );
 	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
 	return <div { ...innerBlocksProps } />;
