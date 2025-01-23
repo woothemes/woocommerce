8abcfff015ecf543047626d1a35ca82137869c06
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2025-01-23 12:27:54 +0800

    Only check the 'Learn about marketing a store' card on non-wpcom sites.

--- plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/overview.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/overview.spec.js
@@ -48,11 +48,10 @@ test.describe( 'Marketing page', () => {
 			await expect(
 				page.getByRole( 'tab', { name: 'CRM', exact: true } )
 			).toBeVisible();
+			await expect(
+				page.getByText( 'Learn about marketing a store' )
+			).toBeVisible();
 		}
-
-		await expect(
-			page.getByText( 'Learn about marketing a store' )
-		).toBeVisible();
 	} );
 
 	test(
