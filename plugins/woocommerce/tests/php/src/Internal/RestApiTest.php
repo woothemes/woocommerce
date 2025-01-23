2e65e46b8faa51d22ff5a86780758379cc647de5
Luigi Teschio <gigitux@gmail.com>
2025-01-23 10:15:03 +0100

    improve code

--- plugins/woocommerce/tests/php/src/Internal/RestApiTest.php
+++ plugins/woocommerce/tests/php/src/Internal/RestApiTest.php
@@ -2,26 +2,16 @@
 
 namespace Automattic\WooCommerce\Tests\Internal;
 
-use Automattic\WooCommerce\Internal\RestApiParameterUtil;
-
 /**
- * Tests for the RestApiParameterUtil class.
+ * Tests for the RestApiTest class.
  * @package Automattic\WooCommerce\Tests\Internal
  */
 class RestApiTest extends \WC_Unit_Test_Case {
 
 	/**
-	 * @testdox 'adjust_create_refund_request_parameters' adjust the 'reason' parameter to null if it's somehow empty.
-	 *
-	 * @testWith ["", null]
-	 *           ["none", null]
-	 *           ["foo", "foo"]
-	 *
-	 * @param string $input_reason The value of 'reason' to test.
-	 * @param string $expected_output_reason The expected converted value of 'reason'.
+	 * Test that the rest api returns false when it is not an rest api request.
 	 */
-	public function test_rest_api_returns_false( $input_reason, $expected_output_reason ) {
-
+	public function test_rest_api_returns_false() {
 		$this->assertEquals( WC()->is_rest_api_request(), false );
 	}
 }
