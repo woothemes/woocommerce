bd189366e1074045f6546c8a052c729a57ead84e
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2025-01-24 11:20:38 -0800

    Add unit tests for send_email endpoint

--- plugins/woocommerce/tests/php/src/Internal/Orders/OrderActionsRestControllerTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Orders/OrderActionsRestControllerTest.php
@@ -20,7 +20,7 @@ class OrderActionsRestControllerTest extends WC_REST_Unit_Test_Case {
 	protected $controller;
 
 	/**
-	 * @var int User ID.
+	 * @var int[] Associative array of user IDs.
 	 */
 	private $user = array();
 
@@ -140,9 +140,9 @@ class OrderActionsRestControllerTest extends WC_REST_Unit_Test_Case {
 	 *
 	 * @dataProvider provide_data_for_email_templates
 	 *
-	 * @param string $user
-	 * @param array $order_props
-	 * @param array $response_data
+	 * @param string $user          Which user from the $users array to use.
+	 * @param array  $order_props   Properties of the order.
+	 * @param array  $response_data Expected results.
 	 *
 	 * @return void
 	 */
@@ -191,8 +191,168 @@ class OrderActionsRestControllerTest extends WC_REST_Unit_Test_Case {
 		}
 	}
 
+	/**
+	 * Data provider for `test_send_email`.
+	 *
+	 * @return \Generator
+	 */
+	public function provide_data_for_send_email() {
+		yield 'unauthorized request' => array(
+			'customer',
+			array(),
+			array(),
+			array(
+				'status'  => 403,
+				'message' => 'Sorry, you cannot create resources.',
+				'notes'   => array(),
+			),
+		);
+		yield 'unavailable template' => array(
+			'shop_manager',
+			array(),
+			array(
+				'template_id' => 'customer_failed_order',
+			),
+			array(
+				'status'  => 400,
+				'message' => 'customer_failed_order is not a valid template for this order.',
+				'notes'   => array(),
+			),
+		);
+		yield 'no billing email' => array(
+			'shop_manager',
+			array(
+				'billing_email' => '',
+			),
+			array(),
+			array(
+				'status'  => 400,
+				'message' => 'Order does not have an email address.',
+				'notes'   => array(),
+			),
+		);
+		yield 'no billing email, email specified, no force' => array(
+			'shop_manager',
+			array(
+				'billing_email' => '',
+			),
+			array(
+				'email' => 'another@example.org'
+			),
+			array(
+				'status'  => 200,
+				'message' => 'Billing email updated to another@example.org. Email template &quot;Completed order&quot; sent to another@example.org, via some app.',
+				'notes'   => array(
+					'Billing email updated to another@example.org.',
+					'Email template &quot;Completed order&quot; sent to another@example.org, via some app.'
+				),
+			),
+		);
+		yield 'existing billing email, email specified, no force' => array(
+			'shop_manager',
+			array(),
+			array(
+				'email' => 'another@example.org'
+			),
+			array(
+				'status'  => 500,
+				'message' => 'Order already has a billing email.',
+				'notes'   => array(),
+			),
+		);
+		yield 'existing billing email, email specified, force' => array(
+			'shop_manager',
+			array(),
+			array(
+				'email' => 'another@example.org',
+				'force' => true,
+			),
+			array(
+				'status'  => 200,
+				'message' => 'Billing email updated to another@example.org. Email template &quot;Completed order&quot; sent to another@example.org, via some app.',
+				'notes'   => array(
+					'Billing email updated to another@example.org.',
+					'Email template &quot;Completed order&quot; sent to another@example.org, via some app.'
+				),
+			),
+		);
+		yield 'completed order' => array(
+			'shop_manager',
+			array(),
+			array(),
+			array(
+				'status'  => 200,
+				'message' => 'Email template &quot;Completed order&quot; sent to customer@example.org, via some app.',
+				'notes'   => array(
+					'Email template &quot;Completed order&quot; sent to customer@example.org, via some app.'
+				),
+			),
+		);
+	}
 
-	//public function test_send_email
+	/**
+	 * Test the wc/v3/orders/{id}/actions/send_email endpoint.
+	 *
+	 * @dataProvider provide_data_for_send_email
+	 *
+	 * @param string $user           Which user from the $users array to use.
+	 * @param array  $order_props    Properties of the order.
+	 * @param array  $request_params Request parameters.
+	 * @param array  $result         Expected results.
+	 *
+	 * @return void
+	 */
+	public function test_send_email( string $user, array $order_props, array $request_params, array $result ) {
+		$order_defaults = array(
+			'billing_email'  => 'customer@example.org',
+			'status'         => 'completed',
+			'partial_refund' => false,
+		);
+		$order_props = wp_parse_args( $order_props, $order_defaults );
+
+		$request_defaults = array(
+			'template_id' => 'customer_completed_order',
+			'email'       => '',
+			'force'       => false,
+		);
+		$request_params = wp_parse_args( $request_params, $request_defaults );
+
+		$order = wc_create_order();
+		if ( true === $order_props['partial_refund'] ) {
+			$order = WC_Helper_Order::create_order();
+			$this->do_partial_refund( $order );
+		}
+
+		$order->set_billing_email( $order_props['billing_email'] );
+		$order->set_status( $order_props['status'] );
+		$order->save();
+
+		wp_set_current_user( $this->user[ $user ] );
+
+		$request = new WP_REST_Request( 'POST', '/wc/v3/orders/' . $order->get_id() . '/actions/send_email' );
+		$request->add_header( 'User-Agent', 'some app' );
+		$request->set_param( 'template_id', $request_params['template_id'] );
+		if ( ! empty( $request_params['email'] ) ) {
+			$request->set_param( 'email', $request_params['email'] );
+		}
+		if ( ! empty( $request_params['force'] ) ) {
+			$request->set_param( 'force_email_update', $request_params['force'] );
+		}
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertEquals( $result['status'], $response->get_status() );
+
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'message', $data );
+		$this->assertEquals( $result['message'], $data['message'] );
+
+		$notes = wc_get_order_notes( array( 'order_id' => $order->get_id() ) );
+		$notes_content = wp_list_pluck( $notes, 'content' );
+		foreach ( $result['notes'] as $note ) {
+			$this->assertContains( $note, $notes_content );
+		}
+	}
 
 	/**
 	 * Test sending order details email.
