8cb3c2f74ac0b65f6ba980c7d0d83ec270f54567
 Ján Mikláš <neosinner@gmail.com>
2025-01-27 22:28:28 +0100

    Bump template version

--- plugins/woocommerce/templates/emails/email-styles.php
+++ plugins/woocommerce/templates/emails/email-styles.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 9.7.0
+ * @version 9.8.0
  */
 
 use Automattic\WooCommerce\Internal\Email\EmailFont;
