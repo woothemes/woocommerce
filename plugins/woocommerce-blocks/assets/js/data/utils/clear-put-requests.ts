73118e652dff1c936f54f4e4b41438022165317a
Alex Florisca <alex.florisca@automattic.com>
2025-01-28 15:02:58 +0000

    Recreate The Abort Controller after it has been used

--- plugins/woocommerce-blocks/assets/js/data/utils/clear-put-requests.ts
+++ plugins/woocommerce-blocks/assets/js/data/utils/clear-put-requests.ts
@@ -4,11 +4,14 @@
 import { clearChanges as clearCheckoutChanges } from '../checkout/push-changes';
 
 // This is used to abort the checkout PUT requests
-export const CheckoutPutAbortController = new AbortController();
+export let CheckoutPutAbortController = new AbortController();
 
 export function clearCheckoutPutRequests(): void {
 	// Abort any requests already in flight (batch not supported yet)
 	CheckoutPutAbortController.abort();
+
+	// Abort controller can only be used once, so we need to create a new one.
+	CheckoutPutAbortController = new AbortController();
 
 	// Clear any debounced requests awaiting to be sent
 	clearCheckoutChanges();
