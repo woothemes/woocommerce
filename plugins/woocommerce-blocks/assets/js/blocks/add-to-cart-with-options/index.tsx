3478078f7092b53f261dd0f106689f52d325c15b
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-01-28 11:06:12 +0100

    Remove Settings panel from Add to Cart with Options block

--- plugins/woocommerce-blocks/assets/js/blocks/add-to-cart-with-options/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/add-to-cart-with-options/index.tsx
@@ -3,9 +3,6 @@
  */
 import { button } from '@wordpress/icons';
 import { getPlugin, registerPlugin } from '@wordpress/plugins';
-import { isExperimentalBlocksEnabled } from '@woocommerce/block-settings';
-import { getSettingWithCoercion } from '@woocommerce/settings';
-import { isBoolean } from '@woocommerce/types';
 import { registerProductBlockType } from '@woocommerce/atomic-utils';
 import type { BlockConfiguration } from '@wordpress/blocks';
 
@@ -17,16 +14,6 @@ import metadata from './block.json';
 import AddToCartOptionsEdit from './edit';
 import './style.scss';
 import type { Attributes } from './types';
-
-// Pick the value of the "blockify add to cart flag"
-const isBlockifiedAddToCart = getSettingWithCoercion(
-	'isBlockifiedAddToCart',
-	false,
-	isBoolean
-);
-
-export const shouldBlockifiedAddToCartWithOptionsBeRegistered =
-	isExperimentalBlocksEnabled() && isBlockifiedAddToCart;
 
 if ( shouldBlockifiedAddToCartWithOptionsBeRegistered ) {
 	// Register a plugin that adds a product type selector to the template sidebar.
