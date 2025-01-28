482b1a1b49af57bef44232046fddf473c1ab2036
Alba Rincón <alba.rincon@automattic.com>
2025-01-28 15:24:20 +0100

    Check if there's a 404 template before using it

--- plugins/woocommerce/includes/wc-template-functions.php
+++ plugins/woocommerce/includes/wc-template-functions.php
@@ -53,7 +53,11 @@ function wc_template_redirect() {
 	if ( is_wc_endpoint_url() && ! is_account_page() && ! is_checkout() && apply_filters( 'woocommerce_account_endpoint_page_not_found', true ) ) {
 		$wp_query->set_404();
 		status_header( 404 );
-		include get_query_template( '404' );
+
+		$template = get_query_template('404');
+		if ( $template ) {
+			include $template;
+		}
 		exit;
 	}
 
