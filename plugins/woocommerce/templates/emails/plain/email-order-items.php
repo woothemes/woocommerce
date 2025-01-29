6341785e6d6150bd60d89a364d38f8f380045ba0
 Ján Mikláš <neosinner@gmail.com>
2025-01-29 10:44:17 +0100

    Improve plain text email order items with string padding

--- plugins/woocommerce/templates/emails/plain/email-order-items.php
+++ plugins/woocommerce/templates/emails/plain/email-order-items.php
@@ -12,12 +12,16 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates\Emails\Plain
- * @version     5.2.0
+ * @version     9.8.0
  */
+
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
 
 foreach ( $items as $item_id => $item ) :
 	if ( apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
@@ -30,14 +34,25 @@ foreach ( $items as $item_id => $item ) :
 			$purchase_note = $product->get_purchase_note();
 		}
 
-		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ) );
-		if ( $show_sku && $sku ) {
-			echo ' (#' . $sku . ')';
+		if ( $email_improvements_enabled ) {
+			$product_name = apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false );
+			$product_name .= ' × ' . apply_filters( 'woocommerce_email_order_item_quantity', $item->get_quantity(), $item );
+			echo wp_kses_post( str_pad( wp_kses_post( $product_name ), 40 ) );
+			echo ' ';
+			echo esc_html( str_pad( wp_kses( $order->get_formatted_line_subtotal( $item ), array() ), 20, ' ', STR_PAD_LEFT ) ) . "\n";
+			if ( $sku ) {
+				echo esc_html( '(#' . $sku . ")\n" );
+			}
+		} else {
+			// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ) );
+			if ( $show_sku && $sku ) {
+				echo ' (#' . $sku . ')';
+			}
+			echo ' X ' . apply_filters( 'woocommerce_email_order_item_quantity', $item->get_quantity(), $item );
+			echo ' = ' . $order->get_formatted_line_subtotal( $item ) . "\n";
+			// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
-		echo ' X ' . apply_filters( 'woocommerce_email_order_item_quantity', $item->get_quantity(), $item );
-		echo ' = ' . $order->get_formatted_line_subtotal( $item ) . "\n";
-		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		// allow other plugins to add additional product information here.
 		do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, $plain_text );
