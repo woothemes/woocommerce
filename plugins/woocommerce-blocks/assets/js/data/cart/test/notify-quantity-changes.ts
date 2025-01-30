38f92d9ff37ca80409c3cfe04f8b4084acee82df
Thomas Roberts <thomas.roberts@automattic.com>
2025-01-30 15:13:32 +0000

    Update notifyQuantityChanges unit test

--- plugins/woocommerce-blocks/assets/js/data/cart/test/notify-quantity-changes.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/test/notify-quantity-changes.ts
@@ -95,6 +95,18 @@ describe( 'notifyQuantityChanges', () => {
 		} );
 		expect( mockedCreateInfoNotice ).not.toHaveBeenCalled();
 	} );
+	it( 'does not show notices when the item is being added', () => {
+		const { oldCart, newCart } = getFreshCarts();
+		newCart.items[ 0 ].quantity = 5;
+		newCart.items[ 0 ].quantity_limits.maximum = 10;
+		notifyQuantityChanges( {
+			oldCart,
+			newCart,
+			cartItemsPendingQuantity: [],
+			productsPendingAdd: [ 1 ],
+		} );
+		expect( mockedCreateInfoNotice ).not.toHaveBeenCalled();
+	} );
 	it( 'does not show notices when a deleted item is the one being removed', () => {
 		const { oldCart, newCart } = getFreshCarts();
 
