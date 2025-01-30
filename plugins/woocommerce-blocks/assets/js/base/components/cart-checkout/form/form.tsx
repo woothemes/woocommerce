56853408a0658e8ea7f4b8caca95ab4eb15206d4
Nadir Seghir <nadir.seghir@gmail.com>
2025-01-30 11:27:52 +0100

    fix import

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/form/form.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/form/form.tsx
@@ -27,7 +27,7 @@ import fastDeepEqual from 'fast-deep-equal/es6';
  * Internal dependencies
  */
 import { AddressFormProps } from './types';
-import { useFormFields } from './use-form-address';
+import { useFormFields } from './use-form-fields';
 import validateCountry from './validate-country';
 import customValidationHandler from './custom-validation-handler';
 import AddressLineFields from './address-line-fields';
