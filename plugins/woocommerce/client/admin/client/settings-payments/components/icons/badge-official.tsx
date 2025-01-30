da5957bd0247b8c27c7e07b8eca895b0b3230228
Vlad Olaru <vlad@pixelgrade.com>
2025-01-30 17:52:30 +0200

    Add official badge icon

---
+++ plugins/woocommerce/client/admin/client/settings-payments/components/icons/badge-official.tsx
@@ -0,0 +1,16 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { SVG, Path } from '@wordpress/primitives';
+
+export default (
+	<SVG xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+		<Path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M13.4779 1.88431L8 0.666992L2.52205 1.88431C2.21702 1.9521 2 2.22264 2 2.53511V9.19293C2 10.5303 2.66841 11.7793 3.7812 12.5211L8 15.3337L12.2188 12.5211C13.3316 11.7793 14 10.5303 14 9.19293V2.53511C14 2.22264 13.783 1.9521 13.4779 1.88431ZM10.9683 5.48145L11.9111 6.42425L7.6684 10.6669L4.84 7.83849L5.78281 6.89562L7.66794 8.78082L10.9683 5.48145Z"
+			fill="#00A32A"
+		/>
+	</SVG>
+);
---
+++ plugins/woocommerce/client/admin/client/settings-payments/components/icons/index.ts
@@ -0,0 +1,1 @@
+export { default as BadgeOfficial } from './badge-official';
